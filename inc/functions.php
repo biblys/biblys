84326cd3653b76dcceef2192d685089a2b3d92e0
Clément Bourgoin <c@iwzr.fr>
2020-09-14 14:11:04 +0200

    Fix mix up when importing a noosfere collection that already exists

--- inc/functions.php
+++ inc/functions.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 // Biblys version
-define('BIBLYS_VERSION', '2.49.2-dev');
+define('BIBLYS_VERSION', '2.49.2-dev.1');
 
 function biblys_error($level, $message, $file, $line, $trace, Exception $exception = null)
 {
