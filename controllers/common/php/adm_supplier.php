4487c570c40725439d610a8ceb0369b1feb2b008
Clément Bourgoin <c@iwzr.fr>
2020-10-21 11:42:01 +0200

    Fix disabling "on order" option for supplier

--- controllers/common/php/adm_supplier.php
+++ controllers/common/php/adm_supplier.php
@@ -1,112 +1,117 @@
 <?php
 
-	$sm = new SupplierManager();
+$sm = new SupplierManager();
 
-	$_PAGE_TITLE = 'Nouveau fournisseur';
+$_PAGE_TITLE = 'Nouveau fournisseur';
 
-	if (isset($_GET['delete']))
+if (isset($_GET['delete']))
+{
+	$supplier = $sm->getById($_GET['id']);
+	if ($supplier)
 	{
-		$supplier = $sm->getById($_GET['id']);
-		if ($supplier)
-		{
-			$sm->delete($supplier);
-			redirect('/pages/adm_suppliers', array('deleted' => $supplier->get('name')));
-		}
+		$sm->delete($supplier);
+		redirect('/pages/adm_suppliers', array('deleted' => $supplier->get('name')));
+	}
+}
+
+if ($request->getMethod() === 'POST') {
+
+
+	if (empty($_POST["supplier_id"])) {
+		$supplier = $sm->create();
+		$_POST['supplier_id'] = $supplier->get('id');
+		$params['created'] = 1;
+	} else {
+		$supplier = $sm->getById($_POST['supplier_id']);
+		$params['updated'] = 1;
 	}
 
-	if ($_SERVER['REQUEST_METHOD'] == 'POST')
-	{
+	foreach ($_POST as $key => $val) {
 		$params = array();
 
-		if (empty($_POST["supplier_id"]))
-		{
-			$supplier = $sm->create();
-			$_POST['supplier_id'] = $supplier->get('id');
-			$params['created'] = 1;
-		}
-		else
-		{
-			$supplier = $sm->getById($_POST['supplier_id']);
-			$params['updated'] = 1;
-		}
-
-		foreach ($_POST as $key => $val)
-		{
-			$supplier->set($key, $val);
-		}
-		$sm->update($supplier);
-
-		// Updated linked articles
-		$params['articles_to_update'] = 0;
-		$articles = $_SQL->query("SELECT `articles`.`article_id` FROM `articles` JOIN `publishers` USING(`publisher_id`) JOIN `links` USING(`publisher_id`) WHERE `supplier_id` = '".$_POST["supplier_id"]."'");
-		while ($a = $articles->fetch(PDO::FETCH_ASSOC))
-		{
-			$_SQL->exec("UPDATE `articles` SET `article_keywords_generated` = NULL, `article_updated` = NOW() WHERE `article_id` = ".$a["article_id"]." LIMIT 1");
-			$params['articles_to_update']++;
-		}
-
-		$params['id'] = $supplier->get('id');
-		redirect('/pages/adm_supplier', $params);
-
+		$supplier->set($key, $val);
 	}
 
-	$supplier = new Supplier(array());
-	if (isset($_GET['id']))
-	{
-		$supplier = $sm->getById($_GET['id']);
+	// Checkboxes
+	$noTax = $request->request->get("supplier_notva") ? 1 : 0;
+	$onOrder = $request->request->get("supplier_on_order") ? 1 : 0;
 
-		if (!$supplier)
-		{
-			trigger_error('Fournisseur inexistant.');
-		}
+	$supplier->set('supplier_notva', $noTax);
+	$supplier->set('supplier_on_order', $onOrder);
 
-		$_PAGE_TITLE = 'Modifier le fournisseur '.$supplier->get('name');
+	$sm->update($supplier);
+
+	// Updated linked articles
+	$params['articles_to_update'] = 0;
+	$articles = $_SQL->query("SELECT `articles`.`article_id` FROM `articles` JOIN `publishers` USING(`publisher_id`) JOIN `links` USING(`publisher_id`) WHERE `supplier_id` = '".$_POST["supplier_id"]."'");
+	while ($a = $articles->fetch(PDO::FETCH_ASSOC)) {
+		$_SQL->exec("UPDATE `articles` SET `article_keywords_generated` = NULL, `article_updated` = NOW() WHERE `article_id` = ".$a["article_id"]." LIMIT 1");
+		$params['articles_to_update']++;
 	}
 
-	$alerts = null;
-	if (isset($_GET["created"])) $alerts .= '<p class="success">Le nouveau fournisseur a été créé.</p>';
-	if (isset($_GET["updated"])) $alerts .= '<p class="success">Le fournisseur a été mis à jour.</p>';
-	if (isset($_GET["articles_to_update"])) $alerts .= '<p class="warning">'.$_GET['articles_to_update'].' articles devront être <a href="/pages/adm_keywords">mis à jour</a>.</p>';
+	$params['id'] = $supplier->get('id');
+	redirect('/pages/adm_supplier', $params);
 
-	$_ECHO .='
-		<h1><span class="fa fa-truck"></span> '.$_PAGE_TITLE.'</h1>
+}
 
-		<div class="admin">
-			<p><a href="/pages/adm_suppliers">Fournisseurs</a></p>
-		</div>
+$supplier = new Supplier(array());
+if (isset($_GET['id'])) {
+	$supplier = $sm->getById($_GET['id']);
 
-		'.$alerts.'
+	if (!$supplier) {
+		trigger_error('Fournisseur inexistant.');
+	}
 
-		<form action="/pages/adm_supplier" method="post" class="fieldset">
-			<fieldset>
-				<legend>Informations</legend>
-				<p>
-					<label for="supplier_id" class="disabled">Fournisseur n&deg;</label>
-					<input type="text" name="supplier_id" id="supplier_name" value="'.$supplier->get('id').'" class="nano" readonly>
-				</p>
+	$_PAGE_TITLE = 'Modifier le fournisseur '.$supplier->get('name');
+}
 
-				<p>
-					<label for="supplier_name" class="required">Nom :</label>
-					<input type="text" name="supplier_name" id="supplier_name" value="'.$supplier->get('name').'" required>
-				</p>
-			</fieldset>
-			<fieldset>
-				<legend>Options</legend>
-				<p>
-					<input type="checkbox" name="supplier_notva" id="supplier_notva" value="1" '.($supplier->has('notva') ? 'checked' : null).'>
-					<label class="after" for="supplier_notva">Exempt&eacute; de TVA</label>
-				</p>
+$alerts = null;
 
-				<p>
-					<input type="checkbox" name="supplier_on_order" id="supplier_on_order" value="1" '.($supplier->has('on_order') ? 'checked' : null).'>
-					<label class="after" for="supplier_on_order">Livres vendables sur commande</label>
-				</p>
-			</fieldset>
-			<fieldset class="center">
-				<button type="submit" class="btn btn-primary"><i class="fa fa-floppy-o"></i> Enregistrer le fournisseur</button>
-				<a href="/pages/adm_supplier?id='.$supplier->get('id').'&delete" class="btn btn-danger" data-confirm="Voulez-vous vraiment SUPPRIMER le fournisseur '.$supplier->get('name').' ?"><i class="fa fa-trash-o"></i> Supprimer</a>
-			</fieldset>
-		</form>
-	';
+if (isset($_GET["created"])) $alerts .= '<p class="success">Le nouveau fournisseur a été créé.</p>';
+if (isset($_GET["updated"])) $alerts .= '<p class="success">Le fournisseur a été mis à jour.</p>';
 
-?>
+if (isset($_GET["articles_to_update"])) {
+	$searchTermsUrl = $urlgenerator->generate('article_search_terms');
+	$alerts .= '<p class="warning">'.$_GET['articles_to_update'].' articles devront être <a href="'.$searchTermsUrl.'">mis à jour</a>.</p>';
+}
+
+$_ECHO .='
+	<h1><span class="fa fa-truck"></span> '.$_PAGE_TITLE.'</h1>
+
+	<div class="admin">
+		<p><a href="/pages/adm_suppliers">Fournisseurs</a></p>
+	</div>
+
+	'.$alerts.'
+
+	<form action="/pages/adm_supplier" method="post" class="fieldset">
+		<fieldset>
+			<legend>Informations</legend>
+			<p>
+				<label for="supplier_id" class="disabled">Fournisseur n&deg;</label>
+				<input type="text" name="supplier_id" id="supplier_name" value="'.$supplier->get('id').'" class="nano" readonly>
+			</p>
+
+			<p>
+				<label for="supplier_name" class="required">Nom :</label>
+				<input type="text" name="supplier_name" id="supplier_name" value="'.$supplier->get('name').'" required>
+			</p>
+		</fieldset>
+		<fieldset>
+			<legend>Options</legend>
+			<p>
+				<input type="checkbox" name="supplier_notva" id="supplier_notva" value="1" '.($supplier->has('notva') ? 'checked' : null).'>
+				<label class="after" for="supplier_notva">Exempt&eacute; de TVA</label>
+			</p>
+
+			<p>
+				<input type="checkbox" name="supplier_on_order" id="supplier_on_order" value="1" '.($supplier->has('on_order') ? 'checked' : null).'>
+				<label class="after" for="supplier_on_order">Livres vendables sur commande</label>
+			</p>
+		</fieldset>
+		<fieldset class="center">
+			<button type="submit" class="btn btn-primary"><i class="fa fa-floppy-o"></i> Enregistrer le fournisseur</button>
+			<a href="/pages/adm_supplier?id='.$supplier->get('id').'&delete" class="btn btn-danger" data-confirm="Voulez-vous vraiment SUPPRIMER le fournisseur '.$supplier->get('name').' ?"><i class="fa fa-trash-o"></i> Supprimer</a>
+		</fieldset>
+	</form>
+';
