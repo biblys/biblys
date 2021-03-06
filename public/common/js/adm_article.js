430e910bd5dcccc5e3f2b53f91049c7f7bbb0efc
Clément Bourgoin <c@iwzr.fr>
2021-03-06 21:37:07 +0100

    fix: displaying article form where there are no rayons

--- public/common/js/adm_article.js
+++ public/common/js/adm_article.js
@@ -770,7 +770,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Add a rayon
 
   var rayonInput = document.querySelector('#rayon_id');
-  rayonInput.addEventListener('change', function () {
+  rayonInput && rayonInput.addEventListener('change', function () {
 
     var rayonId = rayonInput.value;
 
