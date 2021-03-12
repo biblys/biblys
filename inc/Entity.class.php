962478a69963847964d692fd98fd1b3ec412e381
Clément Bourgoin <c@iwzr.fr>
2021-03-12 16:51:36 +0100

    fix: counting publishers with site filter

--- inc/Entity.class.php
+++ inc/Entity.class.php
@@ -336,13 +336,17 @@ class EntityManager
             $where['site_id'] = $site->get('id');
         }
 
+        if (method_exists($this, 'addSiteFilters')) {
+            $where = $this->addSiteFilters($where);
+        }
+
         $q = EntityManager::buildSqlQuery($where);
-        
+
         $queryWhere = '';
         if ($q['where']) {
             $queryWhere = ' AND '.$q['where'];
         }
-        
+
         $query = 'SELECT COUNT(*) FROM `'.$this->table.'` WHERE `'.$this->prefix.'_deleted` IS NULL '.$queryWhere;
         $res = EntityManager::prepareAndExecute($query, $q['params']);
 
