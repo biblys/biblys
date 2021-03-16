bbf2d0d8e72f4d92c7719c8489235c3dabc678a7
Clément Bourgoin <c@iwzr.fr>
2021-03-16 23:24:47 +0100

    feat: reset test database before running tests

--- tests/setUp.php
+++ tests/setUp.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('display_errors', 'On');
+ini_set("display_errors", "On");
 error_reporting(E_ALL);
 
 $_SERVER = [
@@ -10,6 +10,25 @@ $_SERVER = [
     "SCRIPT_NAME" => "index.php"
 ];
 
+// Load database config
+require_once "inc/constants.php";
+$config = new Biblys\Utils\Config();
+$dbConfig = $config->get("db");
+if (!isset($dbConfig["test"])) {
+    throw new Exception("Missing test database config!");
+}
+
+// Connect to test
+$testDb = Biblys\Database\Connection::init($dbConfig["test"]);
+
+// Reset test database
+$testBaseName = $dbConfig["test"]["base"];
+$testDb->exec("DROP DATABASE IF EXISTS `$testBaseName`");
+$testDb->exec("CREATE DATABASE `$testBaseName`");
+$testDb->exec("USE `$testBaseName`");
+$sql = file_get_contents("db-schema.sql");
+$testDb->exec($sql);
+
 global $config;
-require_once('inc/functions.php');
-$config->set('environment', 'test');
+require_once "inc/functions.php";
+$config->set("environment", "test");
