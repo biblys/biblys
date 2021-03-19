3e30a3dc726030828d4d232ca6a020e968cb1b2c
Clément Bourgoin <c@iwzr.fr>
2021-03-19 12:26:01 +0100

    style: fix adm_publisher_stock indentation

--- controllers/common/php/adm_publisher_stock.php
+++ controllers/common/php/adm_publisher_stock.php
@@ -1,58 +1,58 @@
 <?php
 
-    $_JS_CALLS[] = "/common/js/adm_publisher_stock.js";
-    $_JS_CALLS[] = "/common/js/sorttable.js";
+$_JS_CALLS[] = "/common/js/adm_publisher_stock.js";
+$_JS_CALLS[] = "/common/js/sorttable.js";
 
-    $_PAGE_TITLE = 'Gestion du stock';
+$_PAGE_TITLE = 'Gestion du stock';
 
-    $am = new ArticleManager();
-    $articles = $am->getAll(
-        ["publisher_id" => $site->get("publisher_id")],
-        ["order" => "article_pubdate", "sort" => "desc"]
-    );
+$am = new ArticleManager();
+$articles = $am->getAll(
+    ["publisher_id" => $site->get("publisher_id")],
+    ["order" => "article_pubdate", "sort" => "desc"]
+);
 
-    // Minimal virtual stock
-    $minimum = 3;
-    $site_minimum = $site->getOpt('minimum_virtual_stock');
-    if ($site_minimum) {
-        $minimum = $site_minimum;
+// Minimal virtual stock
+$minimum = 3;
+$site_minimum = $site->getOpt('minimum_virtual_stock');
+if ($site_minimum) {
+    $minimum = $site_minimum;
+}
+
+$collections = [];
+foreach ($articles as $article) {
+    $td_class = null;
+
+    if ($article->get("publisher_stock") <= $minimum) {
+        $td_class = " alert";
     }
 
-    $collections = [];
-    foreach ($articles as $article) {
-        $td_class = null;
+    if ($article->isSoldout() || !$article->isPhysical()) {
+        continue;
+    }
 
-        if ($article->get("publisher_stock") <= $minimum) {
-            $td_class = " alert";
-        }
+    if (!$article->isPublished() && !$article->isPreorderable()) {
+        continue;
+    }
 
-        if ($article->isSoldout() || !$article->isPhysical()) {
-            continue;
-        }
+    $collection = $article->get("collection")->get("name");
+    if (!array_key_exists($collection, $collections)) {
+        $collections[$collection] = null;
+    }
 
-        if (!$article->isPublished() && !$article->isPreorderable()) {
-            continue;
-        }
-
-        $collection = $article->get("collection")->get("name");
-        if (!array_key_exists($collection, $collections)) {
-            $collections[$collection] = null;
-        }
-
-        $collections[$collection] .= '
-            <tr id="article_'.$article->get("id").'">
-                <td sorttable_customkey="'.$article->get("title_alphabetic").'"><a href="/'.$article->get("url").'">'.$article->get("title").'</a></td>
-                <td sorttable_customkey="'.$article->get("authors_alphabetic").'">'.authors($article->get("authors"), 'url').'</td>
-                <td class="right stock'.$td_class.'" data-title="'.$article->get("title").'" data-id="'.$article->get("id").'" data-stock="'.$article->get("publisher_stock").'" contenteditable>'.$article->get("publisher_stock").'</td>
+    $collections[$collection] .= '
+            <tr id="article_' . $article->get("id") . '">
+                <td sorttable_customkey="' . $article->get("title_alphabetic") . '"><a href="/' . $article->get("url") . '">' . $article->get("title") . '</a></td>
+                <td sorttable_customkey="' . $article->get("authors_alphabetic") . '">' . authors($article->get("authors"), 'url') . '</td>
+                <td class="right stock' . $td_class . '" data-title="' . $article->get("title") . '" data-id="' . $article->get("id") . '" data-stock="' . $article->get("publisher_stock") . '" contenteditable>' . $article->get("publisher_stock") . '</td>
             </tr>
         ';
-    }
+}
 
-    $_ECHO .= '<h2>'.$_PAGE_TITLE.'</h2>';
+$_ECHO .= '<h2>' . $_PAGE_TITLE . '</h2>';
 
-    foreach ($collections as $collection => $articles) {
-        $_ECHO .= '
-            <h3>'.$collection.'</h3>
+foreach ($collections as $collection => $articles) {
+    $_ECHO .= '
+            <h3>' . $collection . '</h3>
             <table class="sortable admin-table publisher_stock" cellpadding=0 cellspacing=0>
                 <thead class="pointer">
                     <tr>
@@ -62,8 +62,8 @@
                     </tr>
                 </thead>
                 <tbody>
-                    '.$articles.'
+                    ' . $articles . '
                 </tbody>
             </table>
         ';
-    }
+}
