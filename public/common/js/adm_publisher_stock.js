cda87389a67470bd8c97340d2f8b15bbf006b54a
Clément Bourgoin <c@iwzr.fr>
2021-03-21 13:24:59 +0100

    style: fix js lint errors

--- public/common/js/adm_publisher_stock.js
+++ public/common/js/adm_publisher_stock.js
@@ -1,26 +1,31 @@
-$(document).ready(function() {
-  $('.stock').blur(function() {
-    title = $(this).data('title');
-    id = $(this).data('id');
-    stock = $(this).html();
-    var n = notify(
+// eslint-disable-next-line strict
+'use strict';
+
+window.jQuery(document).ready(function() {
+  window.jQuery('.stock').blur(function() {
+    const title = window.jQuery(this).data('title');
+    const id = parseInt(window.jQuery(this).data('id'));
+    const stock = parseInt(window.jQuery(this).html());
+    var n = window.notify(
       'Mise &agrave; jour du stock de<br />&laquo; ' + title + ' &raquo; en cours...',
       'sticky'
     );
-    $.ajax({
+    window.jQuery.ajax({
       url: '/x/adm_publisher_stock',
       data: {
         article_id: id,
         article_publisher_stock: stock
       },
-			complete: function (jqXHR) {
-				notify('close', n);
+      complete: function (jqXHR) {
+        window.notify('close', n);
         const data = jqXHR.responseJSON;
         if (data.error) {
-          _alert(data.error);
+          window._alert(data.error);
         } else {
-          td = $('#article_' + id + ' > td.stock');
-          if (stock > 3) td.removeClass('alert');
+          const td = window.jQuery('#article_' + id + ' > td.stock');
+          if (stock > 3) {
+            td.removeClass('alert');
+          }
           else td.addClass('alert');
         }
       }
