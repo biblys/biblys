648f68f028eb91921ea77d7e737d22447f80ee66
Clément Bourgoin <c@iwzr.fr>
2021-03-26 16:23:15 +0100

    build: bump version to 2.51.5 & update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+### 2.51.5 (26 mars 2021)
+
+Correction d'une erreur qui pouvait conduire à l'affichage d'une erreur 404
+après l'authentification via Axys.
+
 ### 2.51.4 (19 mars 2021)
 
 Corrections
--- inc/functions.php
+++ inc/functions.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 // Biblys version
-define('BIBLYS_VERSION', '2.51.4');
+define('BIBLYS_VERSION', '2.51.5');
 
 /**
  * Calls biblys_error with the correct arguments for an Exception
