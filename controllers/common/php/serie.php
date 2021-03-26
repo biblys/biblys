04a852a7f2521a6ab33a12a0a6df9dfe4ec12172
Clément Bourgoin <c@iwzr.fr>
2021-03-26 06:03:22 +0100

    feat: redirect from old to new cycle controller

--- controllers/common/php/serie.php
+++ controllers/common/php/serie.php
@@ -1,34 +1,43 @@
 <?php
 
-$cm = new CycleManager();
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-if ($cycle = $cm->get(array('cycle_url' => $_GET['url']))) {
-    $_PAGE_TITLE = 'La série &laquo; '.$cycle->get('name').' &raquo;';
-    
-    $_ECHO .= '<h2>'.$_PAGE_TITLE.'</h2>';
-    
-    if ($cycle->has('desc')) {
-        $_ECHO .= '<p>'.$cycle->get('desc').'</p>';
-    }
-    
-    if ($_V->isAdmin()) {
-        $_ECHO .= '
-			<div class="admin">
-				<p>Cycle n&deg; '.$cycle->get('id').'</p>
-				<p><a href="/pages/adm_cycle?id='.$cycle->get('id').'">modifier</a></p>
-			</div>
-		';
-    }
-    
-    $_REQ = "`cycle_id` = '".$cycle->get('id')."'";
-    
-    // Trier par tome
-    $defaultOrderBy = 'article_tome';
-    $defaultSortOrder = 0;
-    $_REQ_ORDER = 'ORDER BY CAST(`article_tome` AS SIGNED), `article_pubdate`';
-    
-    $path = get_controller_path('_list');
-    include $path;
-} else {
-    $_ECHO = e404();
+$cm = new CycleManager();
+$cycle = $cm->get(array('cycle_url' => $_GET['url']));
+
+if (!$cycle) {
+    throw new NotFoundHttpException();
 }
+
+$useOldController = $site->getOpt('use_old_cycle_controller');
+if (!$useOldController) {
+    return new RedirectResponse("/cycle/".$cycle->get("url"), 301);
+}
+
+$_PAGE_TITLE = 'La série &laquo; '.$cycle->get('name').' &raquo;';
+
+$_ECHO .= '<h2>'.$_PAGE_TITLE.'</h2>';
+
+if ($cycle->has('desc')) {
+    $_ECHO .= '<p>'.$cycle->get('desc').'</p>';
+}
+
+if ($_V->isAdmin()) {
+    $_ECHO .= '
+        <div class="admin">
+            <p>Cycle n&deg; '.$cycle->get('id').'</p>
+            <p><a href="/pages/adm_cycle?id='.$cycle->get('id').'">modifier</a></p>
+        </div>
+    ';
+}
+
+$_REQ = "`cycle_id` = '".$cycle->get('id')."'";
+
+// Trier par tome
+$defaultOrderBy = 'article_tome';
+$defaultSortOrder = 0;
+$_REQ_ORDER = 'ORDER BY CAST(`article_tome` AS SIGNED), `article_pubdate`';
+
+$path = get_controller_path('_list');
+include $path;
