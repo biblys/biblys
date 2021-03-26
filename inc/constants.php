b1812649e52bc34affc7730d16567860d36311b2
Clément Bourgoin <c@iwzr.fr>
2021-03-26 23:44:01 +0100

    build: bump version to 2.52.0-dev.14

--- inc/constants.php
+++ inc/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BIBLYS_VERSION', '2.52.0-dev.13');
+define('BIBLYS_VERSION', '2.52.0-dev.14');
 
 if (!defined('BIBLYS_PATH')) {
     define('BIBLYS_PATH', dirname(dirname(__FILE__)) . '/');
