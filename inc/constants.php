dff18b01330e3feb55e24f381758b838387edaed
Clément Bourgoin <c@iwzr.fr>
2021-03-26 23:33:19 +0100

    Bump version to 2.52.0-dev.13

--- inc/constants.php
+++ inc/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BIBLYS_VERSION', '2.52.0-dev.12');
+define('BIBLYS_VERSION', '2.52.0-dev.13');
 
 if (!defined('BIBLYS_PATH')) {
     define('BIBLYS_PATH', dirname(dirname(__FILE__)) . '/');
