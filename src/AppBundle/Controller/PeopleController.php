ce048923470831480362535a9951b8444286bbc2
Clément Bourgoin <c@iwzr.fr>
2021-09-12 15:03:34 +0200

    feat(web): allow to change gender in people editor

--- src/AppBundle/Controller/PeopleController.php
+++ src/AppBundle/Controller/PeopleController.php
@@ -2,7 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use Exception;
 use Framework\Controller;
+use Framework\Exception\AuthException;
+use PeopleManager;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -10,6 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException as NotFoundException;
 
 class PeopleController extends Controller
@@ -79,33 +84,29 @@ class PeopleController extends Controller
     }
 
     /**
-     * Edit a people
-     * /admin/people/{id}/edit.
-     *
+     * @route /admin/people/{id}/edit.
      * @param int $id
-     *
-     * @return Response
+     * @return RedirectResponse|Response
+     * @throws AuthException
      */
-    public function editAction(Request $request, $id)
+    public function editAction(Request $request, int $id)
     {
-        global $site;
-
         $this->auth('admin');
 
-        $pm = $this->entityManager('People');
-
+        $pm = new PeopleManager();
         $people = $pm->get(['people_id' => $id]);
         if (!$people) {
             throw new NotFoundException("People $id not found.");
         }
 
-        $this->setPageTitle('Modifier le contributeur ' . $people->get('name'));
+        $request->attributes->set("page_title", "Modifier le contributeur ".$people->get('name'));
 
         $formFactory = $this->getFormFactory();
 
         $defaults = [
             'first_name' => $people->get('first_name'),
             'last_name' => $people->get('last_name'),
+            'gender' => $people->get('gender'),
             'bio' => $people->get('bio'),
             'site' => $people->get('site'),
             'facebook' => $people->get('facebook'),
@@ -115,6 +116,16 @@ class PeopleController extends Controller
         $form = $formFactory->createBuilder(FormType::class, $defaults)
             ->add('first_name', TextType::class, ['label' => 'Prénom :', 'required' => false])
             ->add('last_name', TextType::class, ['label' => 'Nom :', 'required' => true])
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Genre à utiliser pour les contributions :',
+                'required' => true,
+                'choices' => [
+                    "" => null,
+                    "Féminin (ex: autrice, illustratrice, etc.)" => "F",
+                    "Masculin (ex: auteur, illustrateur, etc.)" => "M",
+                    "Neutre (ex: auteur·trice, illustrateur·trice, etc.)" => "N",
+                ]
+            ])
             ->add('photo', FileType::class, ['label' => 'Photo (JPEG) :', 'required' => false])
             ->add('site', TextType::class, ['label' => 'Site web :', 'required' => false, 'attr' => ['placeholder' => 'http(s)://']])
             ->add('facebook', TextType::class, ['label' => 'Page Facebook :', 'required' => false, 'attr' => ['placeholder' => 'https://www.facebook.com/...']])
@@ -130,6 +141,7 @@ class PeopleController extends Controller
             $updated = clone $people;
             $updated->set('people_first_name', $data['first_name'])
                 ->set('people_last_name', $data['last_name'])
+                ->set('people_gender', $data['gender'])
                 ->set('people_bio', $data['bio'])
                 ->set('people_site', $data['site'])
                 ->set('people_facebook', $data['facebook'])
@@ -142,7 +154,7 @@ class PeopleController extends Controller
                 if ($data['photo'] !== null) {
                     $updated->addPhoto($data['photo']);
                 }
-            } catch (\Exception $e) {
+            } catch (Exception $e) {
                 $error = $e->getMessage();
             }
 
