9e1268b852235052e2e96ada568e53642e97a71e
Clément Bourgoin <c@iwzr.fr>
2022-02-01 19:52:05 +0100

    refactor(legacy): fix deprecation notice in legacy controller

--- controllers/common/php/_page.php
+++ controllers/common/php/_page.php
@@ -1,10 +1,15 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
+
+/** @var Page $page */
 $_PAGE_TITLE = $page->get("title");
 $errorText = null;
+$content = null;
 
+/** @var Visitor $_V */
 if ($_V->isAdmin()) {
-  $_ECHO .= '
+  $content .= '
     <div class="admin">
       <p>Page n&deg; '.$page->get('id').'</p>
       <p><a href="/pages/adm_page?id='.$page->get('id').'">modifier</a></p>
@@ -14,10 +19,11 @@ if ($_V->isAdmin()) {
 }
 
 if (!isset($_OPENGRAPH)) {
-  $_OPENGRAPH = '
+    /** @var Site $site */
+    $_OPENGRAPH = '
     <meta property="og:title" content="'.$page->get('title').'">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="http://'.$site->get('domain').'/pages/'.$page->get('url').'">
+    <meta property="og:url" content="https://'.$site->get('domain').'/pages/'.$page->get('url').'">
     <meta property="og:description" content="'.truncate(strip_tags($page->get('content')), 500, '...', true).'">
     <meta property="og:locale" content="fr_FR">
     <meta property="og:site_name" content="'.$site->get('title').'">
@@ -51,7 +57,7 @@ if ($page->has('updated')) {
 }
 
 if ($page->get('status') == 0) {
-    $_ECHO .= '
+    $content .= '
         <p class="alert alert-warning">
             <span class="fa fa-warning"></span>
             Cette page est hors-ligne.
@@ -60,7 +66,7 @@ if ($page->get('status') == 0) {
 }
 
 if ($errorText) {
-    $_ECHO = '
+    $content = '
       <div class="alert alert-danger">
         Le code HTML de la page contient des erreurs !
         <ul>'.$errorText.'</ul>
@@ -68,7 +74,7 @@ if ($errorText) {
     ';
 }
 
-$_ECHO .= '
+$content .= '
   <article id="'.$page->get('url').'" class="page">
     <h1 class="page-title">'.$page->get('title').'</h1>
     '.$page->get('content').'
@@ -77,3 +83,5 @@ $_ECHO .= '
     </footer>
   </article>
 ';
+
+return new Response($content);
