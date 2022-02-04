62ae0dbdc4f730d418e3fed9bb9c25a8d15e1adc
Clément Bourgoin <c@iwzr.fr>
2022-02-04 16:13:55 +0100

    style(legacy): fix identation in adm_alerts controller

--- controllers/common/php/adm_alerts.php
+++ controllers/common/php/adm_alerts.php
@@ -1,44 +1,44 @@
 <?php
 
-    $_PAGE_TITLE = 'Livres les plus recherchés';
+$_PAGE_TITLE = 'Livres les plus recherchés';
 
-	$alerts = $_SQL->query("SELECT COUNT(`alert_id`) AS `num`, `article_title`, `article_url`, `article_authors`, `article_collection`, `article_number`, `article_publisher`
-        FROM `alerts`
-        JOIN `articles` USING(`article_id`)
-        GROUP BY `article_id`
-        ORDER BY COUNT(`alert_id`) DESC");
+$alerts = $_SQL->query("SELECT COUNT(`alert_id`) AS `num`, `article_title`, `article_url`, `article_authors`, `article_collection`, `article_number`, `article_publisher`
+    FROM `alerts`
+    JOIN `articles` USING(`article_id`)
+    GROUP BY `article_id`
+    ORDER BY COUNT(`alert_id`) DESC");
 
-   $table = null;
-    while($a = $alerts->fetch()) {
-        $table .= '
-            <tr>
-                <td>
-					<a href="/'.$a["article_url"].'">'.$a["article_title"].'</a><br>
-					'.authors($a["article_authors"]).'
-				</td>
-				<td>'.$a["article_collection"].numero($a["article_number"]).'</td>
-				<td class="center">'.$a["num"].'</td>
-            </tr>
-        ';
-    }
-
-    $_ECHO .= '
-
-        <h1><span class="fa fa-bell"></span> '.$_PAGE_TITLE.'</h1>
-
-        <table class="admin-table">
-            <thead>
-                <tr>
-                    <th>Livre</th>
-                    <th>Collection</th>
-                    <th>Alertes</th>
-                </tr>
-            </thead>
-            <tbody>
-                '.$table.'
-            </tbody>
-        </table>
-
+$table = null;
+while($a = $alerts->fetch()) {
+    $table .= '
+        <tr>
+            <td>
+                <a href="/'.$a["article_url"].'">'.$a["article_title"].'</a><br>
+                '.authors($a["article_authors"]).'
+            </td>
+            <td>'.$a["article_collection"].numero($a["article_number"]).'</td>
+            <td class="center">'.$a["num"].'</td>
+        </tr>
     ';
+}
+
+$_ECHO .= '
+
+    <h1><span class="fa fa-bell"></span> '.$_PAGE_TITLE.'</h1>
+
+    <table class="admin-table">
+        <thead>
+            <tr>
+                <th>Livre</th>
+                <th>Collection</th>
+                <th>Alertes</th>
+            </tr>
+        </thead>
+        <tbody>
+            '.$table.'
+        </tbody>
+    </table>
+
+';
 
 ?>
