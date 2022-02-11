0527d2d2fd76bbcee689a5824fa7b1dcae49e8c5
Clément Bourgoin <c@iwzr.fr>
2022-02-11 11:59:28 +0100

    refactor(web): fix deprecations notices

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -2,32 +2,43 @@
 
 namespace AppBundle\Controller;
 
+use Biblys\Article\Type;
+use Biblys\Service\CurrentSite;
 use Framework\Controller;
 
+use Framework\Exception\AuthException;
+use OptionManager;
+use Propel\Runtime\Exception\PropelException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class SiteController extends Controller
 {
 
     public function __construct()
     {
-        global $urlgenerator, $_V, $_SITE, $session;
+        global $urlgenerator, $_V;
 
-        $this->sm = new \SiteManager();
-        $this->om = new \OptionManager();
-
-        $this->site = $this->sm->getById($_SITE['site_id']);
         $this->user = $_V;
         $this->url = $urlgenerator;
+
+        parent::__construct();
     }
 
-    public function optionsAction(Request $request, Session $session)
+    /**
+     * @throws SyntaxError
+     * @throws AuthException
+     * @throws RuntimeError
+     * @throws PropelException
+     * @throws LoaderError
+     */
+    public function optionsAction(Request $request, Session $session, CurrentSite $currentSite)
     {
-        global $site;
-
-        $this->auth('admin');
+        self::authAdmin($request);
         $request->attributes->set("page_title", "Options du site");
 
         if ($request->getMethod() == "POST") {
@@ -36,59 +47,73 @@ class SiteController extends Controller
             $new_key = $request->request->get('new_key');
             $new_val = $request->request->get('new_value');
             if (!empty($new_key) && !empty($new_val)) {
-                $this->site->setOpt($new_key, $new_val);
-                $session->getFlashbag()->add('success', "L'option &laquo;&nbsp;".$new_key."&nbsp;&raquo; a été ajoutée.");
+                $currentSite->setOption($new_key, $new_val);
+                $session->getFlashbag()->add(
+                    "success",
+                    "L'option &laquo;&nbsp;".$new_key."&nbsp;&raquo; a été ajoutée."
+                );
             }
 
             // Update existing options
-            $options = $request->request->all('options', []);
+            $options = $request->request->all("options");
             foreach ($options as $key => $val) {
-                $this->site->setOpt($key, $val);
+                $currentSite->setOption($key, $val);
             }
 
-            return new RedirectResponse($this->url->generate('site_options'));
+            return new RedirectResponse($this->url->generate("site_options"));
         }
 
-        $options = $this->om->getAll(['site_id' => $site->get('id'), 'user_id' => 'NULL'], ['order' => 'option_key']);
+        $om = new OptionManager();
+        $options = $om->getAll(
+            ["site_id" => $currentSite->getSite()->getId(), "user_id" => "NULL"],
+            ["order" => 'option_key']
+        );
 
-        return $this->render('AppBundle:Site:options.html.twig', [
-            'options' => $options
+        return $this->render("AppBundle:Site:options.html.twig", [
+            "options" => $options
         ]);
     }
 
 
-    public function defaultValuesAction(Request $request)
+    /**
+     * @throws SyntaxError
+     * @throws AuthException
+     * @throws RuntimeError
+     * @throws PropelException
+     * @throws LoaderError
+     */
+    public function defaultValuesAction(Request $request, CurrentSite $currentSite)
     {
-        $this->auth('admin');
-        $this->setPageTitle('Valeurs par défaut');
+        self::authAdmin($request);
+        $request->attributes->set("page_title", "Valeurs par défaut");
 
         if ($request->getMethod() == "POST") {
 
-            $options = $request->request->get('options', []);
+            $options = $request->request->all('options');
             foreach ($options as $key => $val) {
-                $this->site->setOpt($key, $val);
+                $currentSite->setOption($key, $val);
             }
 
             return new RedirectResponse($this->url->generate('site_default_values'));
         }
 
         return $this->render('AppBundle:Site:defaultValues.html.twig', [
-            'type_options' => \Biblys\Article\Type::getOptions($this->site->getOpt('default_type_id')),
+            'type_options' => Type::getOptions($currentSite->getOption('default_type_id')),
             'options' => [
-                'default_collection_id' => $this->site->getOpt('default_collection_id'),
-                'default_article_source_id' => $this->site->getOpt('default_article_source_id'),
-                'default_article_pubdate' => $this->site->getOpt('default_article_pubdate'),
-                'default_article_tags' => $this->site->getOpt('default_article_tags'),
-                'default_stock_invoice' => $this->site->getOpt('default_stock_invoice'),
-                'default_stock_stockage' => $this->site->getOpt('default_stock_stockage'),
-                'default_stock_purchase_price' => $this->site->getOpt('default_stock_purchase_price'),
-                'default_stock_selling_price' => $this->site->getOpt('default_stock_selling_price'),
-                'default_stock_condition' => $this->site->getOpt('default_stock_condition'),
-                'default_stock_condition_details' => $this->site->getOpt('default_stock_condition_details'),
-                'default_stock_discount' => $this->site->getOpt('default_stock_discount'),
-                'default_stock_super_discount' => $this->site->getOpt('default_stock_super_discount'),
-                'default_stock_cascading_discount' => $this->site->getOpt('default_stock_cascading_discount'),
-                'default_stock_purchase_date' => $this->site->getOpt('default_stock_purchase_date')
+                'default_collection_id' => $currentSite->getOption('default_collection_id'),
+                'default_article_source_id' => $currentSite->getOption('default_article_source_id'),
+                'default_article_pubdate' => $currentSite->getOption('default_article_pubdate'),
+                'default_article_tags' => $currentSite->getOption('default_article_tags'),
+                'default_stock_invoice' => $currentSite->getOption('default_stock_invoice'),
+                'default_stock_stockage' => $currentSite->getOption('default_stock_stockage'),
+                'default_stock_purchase_price' => $currentSite->getOption('default_stock_purchase_price'),
+                'default_stock_selling_price' => $currentSite->getOption('default_stock_selling_price'),
+                'default_stock_condition' => $currentSite->getOption('default_stock_condition'),
+                'default_stock_condition_details' => $currentSite->getOption('default_stock_condition_details'),
+                'default_stock_discount' => $currentSite->getOption('default_stock_discount'),
+                'default_stock_super_discount' => $currentSite->getOption('default_stock_super_discount'),
+                'default_stock_cascading_discount' => $currentSite->getOption('default_stock_cascading_discount'),
+                'default_stock_purchase_date' => $currentSite->getOption('default_stock_purchase_date')
             ]
         ]);
     }
