729cee8f30753a3cd0c23eb43440f2644d3c0696
Clément Bourgoin <c@iwzr.fr>
2022-03-22 23:19:11 +0100

    feat(web): add OpenIDConnectController->callback action

--- src/AppBundle/Controller/OpenIDConnectController.php
+++ src/AppBundle/Controller/OpenIDConnectController.php
@@ -5,7 +5,10 @@ namespace AppBundle\Controller;
 use Axys\AxysOpenIDConnectProvider;
 use Biblys\Service\Config;
 use Framework\Controller;
+use OpenIDConnectClient\Exception\InvalidTokenException;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class OpenIDConnectController extends Controller
 {
@@ -18,4 +21,20 @@ class OpenIDConnectController extends Controller
         return new RedirectResponse($provider->getAuthorizationUrl($options));
     }
 
+    /**
+     * @throws InvalidTokenException
+     */
+    public function callback(Request $request, Config $config): RedirectResponse
+    {
+        $axysConfig = $config->get("axys");
+        $provider = new AxysOpenIDConnectProvider($axysConfig);
+        $code = $request->query->get("code");
+        $token = $provider->getAccessToken("authorization_code", ["code" => $code]);
+        $idToken = $token->getIdToken();
+        $idTokenCookie = Cookie::create("id_token")->withValue($idToken);
+        $response = new RedirectResponse("/");
+        $response->headers->setCookie($idTokenCookie);
+
+        return $response;
+    }
 }
