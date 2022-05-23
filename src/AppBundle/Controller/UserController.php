206624dadabc841eb0a1ab21602c0626701c4524
Clément Bourgoin <c@iwzr.fr>
2022-05-23 17:49:48 +0200

    feat(web): add route /user/login
    that redirects to Axys open id login

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Framework\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+
+class UserController extends Controller
+{
+    public function login(UrlGenerator $urlGenerator): Response
+    {
+        $loginRoute = $urlGenerator->generate("openid_axys");
+        return new RedirectResponse($loginRoute);
+    }
+}
---
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+
+class UserControllerTest extends TestCase
+{
+    public function testLogin()
+    {
+        // given
+        $userController = new UserController();
+        $urlGenerator = $this->createMock(UrlGenerator::class);
+        $urlGenerator
+            ->method("generate")
+            ->with("openid_axys")
+            ->willReturn("/openid/axys");
+
+        // when
+        $response = $userController->login($urlGenerator);
+
+        // then
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals("/openid/axys", $response->getTargetUrl());
+    }
+}
