a68f19c7124e5644d3a1071e903b6a0e9ea9c62f
Clément Bourgoin <c@iwzr.fr>
2022-07-12 06:46:37 +0200

    build: bump version to 2.63.0-dev

--- inc/constants.php
+++ inc/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-const BIBLYS_VERSION = "2.62.2";
+const BIBLYS_VERSION = "2.63.0-dev";
 const BIBLYS_PATH = __DIR__."/../";
 const DL_PATH = BIBLYS_PATH . "/../../dl";
 const DL_URL = "http://dl.biblys.fr";
