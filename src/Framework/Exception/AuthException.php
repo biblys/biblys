3ed860ece6f21ae3323e74df433ab3abea56b7a2
Clément Bourgoin <c@iwzr.fr>
2022-07-22 21:31:50 +0200

    cleanup(api): deprecate AuthException

--- src/Framework/Exception/AuthException.php
+++ src/Framework/Exception/AuthException.php
@@ -2,6 +2,9 @@
 
 namespace Framework\Exception;
 
+/**
+ * @deprecated Use UnauthorizedHttpException for 401 and AccessDeniedHttpException for 403
+ */
 class AuthException extends \Exception
 {
     public function __construct($message, $code = 0, Exception $previous = null) 
