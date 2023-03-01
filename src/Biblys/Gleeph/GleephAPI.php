7421725936650f212592edaa89759df37c762e01
Clément Latzarus <c@iwzr.fr>
2023-03-01 23:46:29 +0100

    feat(deps): add gleeph api wrapper

---
+++ src/Biblys/Gleeph/GleephAPI.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Biblys\Gleeph;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class GleephAPI
+{
+    private string $apiKey;
+    private string $environment = "prod";
+    private ClientInterface $client;
+
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+
+        $this->setHttpClient(new Client());
+    }
+
+    /**
+     * @param string $environment
+     * @return GleephAPI
+     */
+    public function setEnvironment(string $environment): GleephAPI
+    {
+        $this->environment = $environment;
+        return $this;
+    }
+
+    /**
+     * @param ClientInterface $client
+     * @return GleephAPI
+     */
+    public function setHttpClient(ClientInterface $client): GleephAPI
+    {
+        $this->client = $client;
+        return $this;
+    }
+
+    /**
+     * @param string $ean
+     * @param int $numberOfSuggestions
+     * @return string[]
+     * @throws ClientExceptionInterface
+     */
+    public function getSimilarBooksByEan(string $ean, int $numberOfSuggestions = 3): array
+    {
+        $queryParams = http_build_query(["ean" => $ean, "nbrecos" => $numberOfSuggestions]);
+        $requestUri = "{$this->_getBaseUrl()}/{$this->_getSimilarBooksByEanEndpointPath()}?$queryParams";
+        $request = new Request("GET", $requestUri, ["x-api-key" => $this->apiKey]);
+        $response = $this->client->sendRequest($request);
+
+        return $this->_extractEansFromResponse($response);
+    }
+
+    private function _getBaseUrl(): string
+    {
+        if ($this->environment === "test") {
+            return "https://test.data.gleeph.pro/v2.0.0";
+        }
+
+        return "https://data.gleeph.pro/v2.0.0";
+    }
+
+    private function _getSimilarBooksByEanEndpointPath(): string
+    {
+        if ($this->environment === "test") {
+            return "similarsbooks_byean_test";
+        }
+
+        return "similarsbooks_byean";
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return array
+     */
+    public function _extractEansFromResponse(ResponseInterface $response): array
+    {
+        $json = json_decode($response->getBody(), true);
+        return array_map(function ($book) {
+            return $book["ean13"];
+        }, $json["similarsbooks"]);
+    }
+}
---
+++ tests/Biblys/Gleeph/GleephAPITest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Biblys\Gleeph;
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+
+class GleephAPITest extends TestCase
+{
+    /**
+     * @throws ClientExceptionInterface
+     */
+    public function testGetSimilarBooksByEan()
+    {
+        // given
+        $gleeph = new GleephAPI("abcd1234");
+        $httpClient = $this->createMock(ClientInterface::class);
+        $response = new Response(200, [], '{ "book_ean13": "3260050120463", "similarsbooks": [ { "ean13": 9782378284923 }, { "ean13": 9782070374670 }, { "ean13": 9782410004793 } ] }');
+        $httpClient
+            ->method("sendRequest")
+            ->with(new Request("GET", "https://data.gleeph.pro/v2.0.0/similarsbooks_byean?ean=9782715233324&nbrecos=10", ["x-api-key" => "abcd1234"]))
+            ->willReturn($response);
+        $gleeph->setHttpClient($httpClient);
+
+        // when
+        $similarBooks = $gleeph->getSimilarBooksByEan("9782715233324", 10);
+
+        // then
+        $this->assertEquals(
+            ["9782378284923", "9782070374670", "9782410004793"],
+            $similarBooks,
+            "returns an array of EANs"
+        );
+    }
+
+
+    /**
+     * @throws ClientExceptionInterface
+     */
+    public function testGetSimilarBooksByEanInTestEnv()
+    {
+        // given
+        $gleeph = new GleephAPI("abcd1234");
+        $httpClient = $this->createMock(ClientInterface::class);
+        $response = new Response(200, [], '{ "book_ean13": "3260050120463", "similarsbooks": [ { "ean13": 9782378284923 }, { "ean13": 9782070374670 }, { "ean13": 9782410004793 } ] }');
+        $httpClient
+            ->method("sendRequest")
+            ->with(new Request("GET", "https://test.data.gleeph.pro/v2.0.0/similarsbooks_byean_test?ean=9782715233324&nbrecos=3", ["x-api-key" => "abcd1234"]))
+            ->willReturn($response);
+        $gleeph->setHttpClient($httpClient);
+        $gleeph->setEnvironment("test");
+
+        // when
+        $similarBooks = $gleeph->getSimilarBooksByEan("9782715233324");
+
+        // then
+        $this->assertEquals(
+            ["9782378284923", "9782070374670", "9782410004793"],
+            $similarBooks,
+            "returns an array of EANs"
+        );
+    }
+}
