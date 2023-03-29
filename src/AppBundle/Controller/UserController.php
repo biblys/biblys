91b45e2a9ea38f619b583f1b59bda56bdc3c295a
Clément Latzarus <c@iwzr.fr>
2023-03-29 10:05:49 +0200

    feat(web): add logged out page

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,10 +3,14 @@
 namespace AppBundle\Controller;
 
 use Framework\Controller;
+use Propel\Runtime\Exception\PropelException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGenerator;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class UserController extends Controller
 {
@@ -15,6 +19,26 @@ class UserController extends Controller
         $returnUrl = $request->query->get("return_url");
         $loginRoute = $urlGenerator->generate("openid_axys", ["return_url" => $returnUrl]);
         return new RedirectResponse($loginRoute);
+    }
+
+    public function logout(UrlGenerator $urlGenerator): Response
+    {
+        $loggedOutUrl = $urlGenerator->generate("user_logged_out");
+        $response = new RedirectResponse($loggedOutUrl, status: 302);
+        $response->headers->clearCookie("user_uid");
+
+        return $response;
+    }
+
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     * @throws PropelException
+     */
+    public function loggedOut(): Response
+    {
+        return $this->render("AppBundle:User:loggedOut.html.twig");
     }
 
     public function signup(): Response
--- tests/AppBundle/Controller/UserControllerTest.php
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -3,8 +3,12 @@
 namespace AppBundle\Controller;
 
 use PHPUnit\Framework\TestCase;
+use Propel\Runtime\Exception\PropelException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGenerator;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 require_once __DIR__."/../../setUp.php";
 
@@ -47,6 +51,28 @@ class UserControllerTest extends TestCase
         // then
         $this->assertEquals(302, $response->getStatusCode());
         $this->assertEquals("openid_axys_url_with_return_url_param", $response->getTargetUrl());
+    }
+
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     * @throws PropelException
+     */
+    public function testLoggedOut()
+    {
+        // given
+        $userController = new UserController();
+
+        // when
+        $response = $userController->loggedOut();
+
+        // then
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString(
+            "Vous avez été déconnecté·e du site Librairie Ys.",
+            $response->getContent(),
+        );
     }
 
     public function testSignup()
