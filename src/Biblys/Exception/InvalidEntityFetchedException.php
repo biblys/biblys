a8bf5106dbb8b3cfdec9e8614c5fcbb1c081e1be
Clément Latzarus <c@iwzr.fr>
2023-04-07 07:33:38 +0200

    refactor(framework): remove default value for argument

--- src/Biblys/Exception/InvalidEntityFetchedException.php
+++ src/Biblys/Exception/InvalidEntityFetchedException.php
@@ -7,12 +7,12 @@ use Exception;
 
 class InvalidEntityFetchedException extends Exception
 {
-    private $invalidEntity = null;
+    private Entity $invalidEntity;
 
-    public function __construct($message = "", string $entityType, Entity $invalidEntity)
+    public function __construct($message, string $entityType, Entity $invalidEntity)
     {
         $message = sprintf("%s %s is invalid: $message", $entityType, $invalidEntity->get("id"));
-        parent::__construct($message, 0, null);
+        parent::__construct($message);
 
         $this->invalidEntity = $invalidEntity;
     }
