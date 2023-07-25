875977cec6688f71851c59b7c06f0efc8d31b7fb
Clément Latzarus <c@iwzr.fr>
2023-07-25 18:08:23 +0200

    refactor(service): extract method

--- src/Biblys/Service/TemplateService.php
+++ src/Biblys/Service/TemplateService.php
@@ -13,6 +13,7 @@ use Symfony\Bridge\Twig\Extension\FormExtension;
 use Symfony\Bridge\Twig\Form\TwigRendererEngine;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Form\FormRenderer;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Csrf\CsrfTokenManager;
@@ -44,9 +45,6 @@ class TemplateService
     }
 
     /**
-     * @param string $templatePath
-     * @param array $vars
-     * @return Response
      * @throws LoaderError
      * @throws RuntimeError
      * @throws SyntaxError
@@ -56,13 +54,66 @@ class TemplateService
     {
         global $request;
 
+        $twig = $this->_getTwigEnvironment($request);
+        $template = $twig->load($templatePath);
+        $rendered = $template->render($vars);
+
+        return new Response($rendered);
+    }
+
+    private function _getTwigEnvironment(Request $request): Environment
+    {
         $config = $this->config;
-        $currentSite = $this->currentSiteService->getSite();
+        $currentSite = $this->currentSiteService;
         $currentUserService = $this->currentUserService;
-        $currentUserIsAdmin = $currentUserService->isAdminForSite($currentSite);
+        $currentUserIsAdmin = $currentUserService->isAdminForSite($currentSite->getSite());
 
-        //** Twig custom functions **//
+        $functions = $this->_getCustomFunctions($config, $currentUserIsAdmin, $currentUserService);
+        $filters = $this->_getCustomFilters();
 
+        $loader = new TemplateLoader($currentSite, new Filesystem());
+        if ($config->get("environment") === "dev") {
+            $twig = new Environment($loader, ['strict_variables' => true]);
+        } else {
+            $twig = new Environment($loader, ['strict_variables' => true, 'debug' => true]);
+            $twig->addExtension(new DebugExtension());
+        }
+
+        $formEngine = new TwigRendererEngine(['AppBundle:Main:_form_bootstrap_layout.html.twig'], $twig);
+        $formRenderer = new FormRenderer($formEngine, new CsrfTokenManager());
+        $twig->addExtension(new FormExtension());
+        $runtimeLoader = new FactoryRuntimeLoader(
+            [
+                FormRenderer::class => function () use ($formRenderer) {
+                    return $formRenderer;
+                },
+            ]
+        );
+        $twig->addRuntimeLoader($runtimeLoader);
+
+        $twig->addGlobal('app', [
+            "currentSite" => $currentSite,
+            "currentUrl" => new CurrentUrlService($request),
+            "currentUser" => $currentUserService,
+            "request" => $request,
+            "user" => new Visitor($request),
+            "session" => new Session(),
+            "site" => new Site($config),
+            "trackers" => self::_getAnalyticsTrackers($config),
+        ]);
+
+        foreach ($functions as $function) {
+            $twig->addFunction($function);
+        }
+
+        foreach ($filters as $filter) {
+            $twig->addFilter($filter);
+        }
+        return $twig;
+    }
+
+    private function _getCustomFunctions(Config $config, bool $currentUserIsAdmin, CurrentUser $currentUserService): array
+    {
         $functions = [];
 
         $functions[] = new TwigFunction("get_assets", function ($fileType) use ($config, $currentUserIsAdmin) {
@@ -116,12 +167,38 @@ class TemplateService
 
             return Cart::buildOneLine($cart->getCount(), $cart->getAmount());
         });
+        return $functions;
+    }
 
-        //** Twig custom filters **//
+    private static function _getAnalyticsTrackers(Config $config): array
+    {
+        $trackers = [];
 
+
+        if ($config->get("matomo.site_id")) {
+            $trackers["matomo"] = [
+                "domain" => $config->get("matomo.domain"),
+                "site_id" => $config->get("matomo.site_id"),
+            ];
+
+            if ($config->get("matomo.secondary_domain")) {
+                $trackers["matomo"]["secondary_domain"] = $config->get("matomo.secondary_domain");
+            }
+        }
+
+        if ($config->get("umami")) {
+            $trackers["umami"] = [
+                "website_id" =>  $config->get("umami.website_id"),
+            ];
+        }
+
+        return $trackers;
+    }
+
+    public function _getCustomFilters(): array
+    {
         $filters = [];
 
-        // authors
         $filters[] = new TwigFilter('authors', function ($authors) {
             $authors = explode(',', $authors);
             $count = count($authors);
@@ -146,12 +223,10 @@ class TemplateService
             return currency($amount, $cents);
         });
 
-        // date
         $filters[] = new TwigFilter('date', function ($date, $format = 'd/m/Y') {
             return _date($date, $format);
         });
 
-        // price
         $filters[] = new TwigFilter('price', function ($price, $currency = null, $decimals = 2) {
             if ($currency == 'EUR') {
                 return number_format(round($price / 100, 6), $decimals, ',', '&#8239;') . '&nbsp;&euro;';
@@ -160,7 +235,6 @@ class TemplateService
             return $price / 100;
         });
 
-        // pluralize
         $filters[] = new TwigFilter('pluralize', function ($text, $number) {
             if ($number == 1 || $number == 0) {
                 return $text;
@@ -169,109 +243,13 @@ class TemplateService
             return $text . 's';
         });
 
-        // truncate
         $filters[] = new TwigFilter('truncate', function ($text, $length, $ellipsis = '…') {
             return truncate(strip_tags($text), $length, $ellipsis);
         });
 
-        // isbn
         $filters[] = new TwigFilter('isbn', function ($ean) {
             return Isbn::convertToIsbn13($ean);
         });
-
-        // Forms
-        $defaultFormTheme = 'AppBundle:Main:_form_bootstrap_layout.html.twig';
-
-        // Custom template loader
-        $currentSite = CurrentSite::buildFromConfig($config);
-        $filesystem = new Filesystem();
-        $loader = new TemplateLoader($currentSite, $filesystem);
-
-        // Load Twig
-        if ($config->get("environment") === "dev") {
-            $twig = new Environment($loader, ['strict_variables' => true]);
-        } else {
-            $twig = new Environment($loader, ['strict_variables' => true, 'debug' => true]);
-            $twig->addExtension(new DebugExtension());
-        }
-
-        // CRSF
-        $session = new Session();
-
-        // Forms
-        $formEngine = new TwigRendererEngine([$defaultFormTheme], $twig);
-        $formRenderer = new FormRenderer($formEngine, new CsrfTokenManager());
-        $twig->addExtension(new FormExtension());
-        $runtimeLoader = new FactoryRuntimeLoader(
-            [
-                FormRenderer::class => function () use ($formRenderer) {
-                    return $formRenderer;
-                },
-            ]
-        );
-        $twig->addRuntimeLoader($runtimeLoader);
-
-        $currentUrlService = new CurrentUrlService($request);
-        $trackers = self::_getAnalyticsTrackers($config);
-
-        // Global variables
-        $app = [
-            "currentSite" => $currentSite,
-            "currentUrl" => $currentUrlService,
-            "currentUser" => $currentUserService,
-            "request" => $request,
-            "user" => new Visitor($request),
-            "session" => $session,
-            "site" => new Site($config),
-            "trackers" => $trackers,
-        ];
-        $twig->addGlobal('app', $app);
-
-        // Import functions
-        foreach ($functions as $function) {
-            $twig->addFunction($function);
-        }
-
-        // Import filters
-        foreach ($filters as $filter) {
-            $twig->addFilter($filter);
-        }
-
-        // Load template file
-        $template = $twig->load($templatePath);
-
-        // Render template
-        $rendered = $template->render($vars);
-
-        return new Response($rendered);
-    }
-
-    /**
-     * @param Config $config
-     * @return array
-     */
-    private static function _getAnalyticsTrackers(Config $config): array
-    {
-        $trackers = [];
-
-
-        if ($config->get("matomo.site_id")) {
-            $trackers["matomo"] = [
-                "domain" => $config->get("matomo.domain"),
-                "site_id" => $config->get("matomo.site_id"),
-            ];
-
-            if ($config->get("matomo.secondary_domain")) {
-                $trackers["matomo"]["secondary_domain"] = $config->get("matomo.secondary_domain");
-            }
-        }
-
-        if ($config->get("umami")) {
-            $trackers["umami"] = [
-                "website_id" =>  $config->get("umami.website_id"),
-            ];
-        }
-
-        return $trackers;
+        return $filters;
     }
 }
