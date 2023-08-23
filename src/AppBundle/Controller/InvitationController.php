8cd1cbb4c6c6310bd9daf567f369d0d6c580b491
Clément Latzarus <c@iwzr.fr>
2023-08-23 12:33:55 +0200

    feat(web): create invitation and send mail

--- src/AppBundle/Controller/InvitationController.php
+++ src/AppBundle/Controller/InvitationController.php
@@ -3,12 +3,25 @@
 namespace AppBundle\Controller;
 
 use Biblys\Article\Type;
+use Biblys\Exception\InvalidEmailAddressException;
 use Biblys\Service\CurrentSite;
+use Biblys\Service\Mailer;
 use Biblys\Service\TemplateService;
+use Exception;
 use Framework\Controller;
 use Model\Base\ArticleQuery;
+use Model\Invitation;
+use Model\Map\ArticleTableMap;
 use Propel\Runtime\Exception\PropelException;
+use Propel\Runtime\Propel;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
@@ -22,10 +35,13 @@ class InvitationController extends Controller
      * @throws PropelException
      */
     public function newAction(
+        Request $request,
         CurrentSite $currentSite,
         TemplateService $templateService,
     ): Response
     {
+        self::authAdmin($request);
+
         $downloadableTypes = Type::getAllDownloadableTypes();
         $downloadbleTypeIds = array_map(function($type) {
             return $type->getId();
@@ -39,5 +55,99 @@ class InvitationController extends Controller
         return $templateService->render("AppBundle:Invitation:new.html.twig", [
             "downloadableArticles" => $downloadableArticles->getData(),
         ]);
+    }
+
+    /**
+     * @throws InvalidEmailAddressException
+     * @throws LoaderError
+     * @throws PropelException
+     * @throws SyntaxError
+     * @throws TransportExceptionInterface
+     */
+    public function createAction(
+        Request         $request,
+        CurrentSite     $currentSite,
+        Mailer          $mailer,
+        TemplateService $templateService,
+        Session         $session,
+        UrlGenerator    $urlGenerator
+    ): RedirectResponse
+    {
+        self::authAdmin($request);
+
+        $recipientEmail = $request->request->get("email_address");
+        if ($recipientEmail === null) {
+            throw new BadRequestHttpException("Le champ adresse e-mail est obligatoire.");
+        }
+
+        $articleId = $request->request->get("article_id");
+        $article = ArticleQuery::create()->filterForCurrentSite($currentSite)->findOneById($articleId);
+        if ($article === null) {
+            throw new BadRequestHttpException("L'article $articleId n'existe pas.");
+        }
+
+        if ($article->getType()->isDownloadable() === false) {
+            throw new BadRequestHttpException("L'article demandé n'est pas téléchargeable.");
+        }
+
+        $invitation = new Invitation();
+        $invitation->setSite($currentSite->getSite());
+        $invitation->setArticle($article);
+        $invitation->setEmail($recipientEmail);
+        $invitation->setCode(Invitation::generateCode());
+        $invitation->setExpiresAt(strtotime("+1 month"));
+
+        $invitationUrl = $urlGenerator->generate("invitation_show", [
+            "code" => $invitation->getCode()
+        ], referenceType: UrlGeneratorInterface::ABSOLUTE_URL);
+        $mailContent = $templateService->renderFromString("
+            <p>Bonjour,</p>
+            <p>
+                Vous avez reçu une invitation à télécharger 
+                <strong>{{ articleTitle }}</strong> 
+                en numérique.<br /> 
+                Suivez le lien ci-dessous pour l’ajouter à votre bibliothèque numérique 
+                <strong>{{ siteTitle }}</strong>
+                d’où vous pourrez le télécharger à volonté dans le format de votre choix.
+            </p>
+            <p>
+                <a href=\"{{ invitationUrl }}\">{{ invitationUrl }}</a>
+            </p>
+            <p>
+                Notez que ce lien n'est valable qu'une seule fois 
+                et expirera le {{ expirationDate }}. 
+            </p>
+            <p>
+                ---<br />
+                {{ siteTitle }} - Propulsé par Biblys
+            </p>
+        ", [
+            "articleTitle" => $article->getTitle(),
+            "siteTitle" => $currentSite->getTitle(),
+            "invitationUrl" => $invitationUrl,
+            "expirationDate" => $invitation->getExpiresAt()->format("d/m/Y")
+        ]);
+
+        $con = Propel::getWriteConnection(ArticleTableMap::DATABASE_NAME);
+        $con->beginTransaction();
+
+        try {
+            $invitation->save();
+            $mailer->send(
+                to: $recipientEmail,
+                subject: "Téléchargez « {$article->getTitle()} » en numérique",
+                body: $mailContent->getContent(),
+            );
+            $con->commit();
+        } catch (Exception $exception) {
+            $con->rollBack();
+            throw $exception;
+        }
+
+        $session->getFlashBag()->add("success",
+            "Une invitation pour {$article->getTitle()} a été envoyée à $recipientEmail"
+        );
+
+        return new RedirectResponse($urlGenerator->generate("invitation_new"));
     }
 }
--- tests/AppBundle/Controller/InvitationControllerTest.php
+++ tests/AppBundle/Controller/InvitationControllerTest.php
@@ -3,12 +3,20 @@
 namespace AppBundle\Controller;
 
 use Biblys\Article\Type;
+use Biblys\Exception\InvalidEmailAddressException;
 use Biblys\Service\CurrentSite;
+use Biblys\Service\Mailer;
 use Biblys\Service\TemplateService;
 use Biblys\Test\ModelFactory;
+use Biblys\Test\RequestFactory;
 use PHPUnit\Framework\TestCase;
 use Propel\Runtime\Exception\PropelException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
@@ -27,9 +35,10 @@ class InvitationControllerTest extends TestCase
     public function testNewAction()
     {
         // given
+        $request = RequestFactory::createAuthRequestForAdminUser();
         $publisher = ModelFactory::createPublisher();
-        ModelFactory::createArticle(publisher: $publisher, title: "Book");
-        $ebook = ModelFactory::createArticle(publisher: $publisher, title: "E-book", typeId: Type::EBOOK);
+        ModelFactory::createArticle(title: "Book", publisher: $publisher);
+        $ebook = ModelFactory::createArticle(title: "E-book", typeId: Type::EBOOK, publisher: $publisher);
         $controller = new InvitationController();
         $site = ModelFactory::createSite();
         $currentSite = new CurrentSite($site);
@@ -44,7 +53,7 @@ class InvitationControllerTest extends TestCase
             ->willReturn(new Response("Créer une invitation de téléchargement"));
 
         // when
-        $response = $controller->newAction($currentSite, $templateService);
+        $response = $controller->newAction($request, $currentSite, $templateService);
 
         // then
         $this->assertEquals(200, $response->getStatusCode());
@@ -52,5 +61,50 @@ class InvitationControllerTest extends TestCase
             "Créer une invitation de téléchargement",
             $response->getContent()
         );
+    }
+
+    /**
+     * @throws LoaderError
+     * @throws PropelException
+     * @throws SyntaxError
+     * @throws InvalidEmailAddressException
+     * @throws TransportExceptionInterface
+     */
+    public function testCreateAction()
+    {
+        // given
+        $request = RequestFactory::createAuthRequestForAdminUser();
+        $request->request->set("email_address", "user@example.org");
+        $flashBag = $this->createMock(FlashBag::class);
+        $flashBag->method("add")->with("success", "Une invitation pour Sent Book a été envoyée à user@example.org");
+        $publisher = ModelFactory::createPublisher();
+        $article = ModelFactory::createArticle(title: "Sent Book", typeId: Type::EBOOK, publisher: $publisher);
+        $request->request->set("article_id", $article->getId());
+        $site = ModelFactory::createSite();
+        $currentSite = new CurrentSite($site);
+        $currentSite->setOption("publisher_filter", $publisher->getId());
+        $session = $this->createMock(Session::class);
+        $session->method("getFlashBag")->willReturn($flashBag);
+        $templateService = $this->createMock(TemplateService::class);
+        $templateService->expects($this->once())->method("renderFromString")->willReturn(new Response("Invitation"));
+        $mailer = $this->createMock(Mailer::class);
+        $mailer->expects($this->once())->method("send");
+        $urlGenerator = $this->createMock(UrlGenerator::class);
+        $urlGenerator->method("generate")->willReturn("/invitation/new");
+        $controller = new InvitationController();
+
+        // when
+        $response = $controller->createAction(
+            request: $request,
+            currentSite: $currentSite,
+            mailer: $mailer,
+            templateService: $templateService,
+            session: $session,
+            urlGenerator: $urlGenerator,
+        );
+
+        // then
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals("/invitation/new", $response->getTargetUrl());
     }
 }
