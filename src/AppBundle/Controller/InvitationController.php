841bb814646ecd7e61ed0c5579b054a397c3060a
Clément Latzarus <c@iwzr.fr>
2023-08-23 11:33:49 +0200

    feat(web): add downloadable invitation create form

--- src/AppBundle/Controller/InvitationController.php
+++ src/AppBundle/Controller/InvitationController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use Biblys\Article\Type;
+use Biblys\Service\CurrentSite;
 use Biblys\Service\TemplateService;
 use Framework\Controller;
+use Model\Base\ArticleQuery;
+use Propel\Runtime\Exception\PropelException;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -15,9 +19,25 @@ class InvitationController extends Controller
      * @throws SyntaxError
      * @throws RuntimeError
      * @throws LoaderError
+     * @throws PropelException
      */
-    public function newAction(TemplateService $templateService): Response
+    public function newAction(
+        CurrentSite $currentSite,
+        TemplateService $templateService,
+    ): Response
     {
-        return $templateService->render("AppBundle:Invitation:new.html.twig");
+        $downloadableTypes = Type::getAllDownloadableTypes();
+        $downloadbleTypeIds = array_map(function($type) {
+            return $type->getId();
+        }, $downloadableTypes);
+
+        $downloadableArticles = ArticleQuery::create()
+            ->filterForCurrentSite($currentSite)
+            ->filterByTypeId($downloadbleTypeIds)
+            ->orderByTitleAlphabetic()
+            ->find();
+        return $templateService->render("AppBundle:Invitation:new.html.twig", [
+            "downloadableArticles" => $downloadableArticles->getData(),
+        ]);
     }
 }
--- tests/AppBundle/Controller/InvitationControllerTest.php
+++ tests/AppBundle/Controller/InvitationControllerTest.php
@@ -2,26 +2,49 @@
 
 namespace AppBundle\Controller;
 
+use Biblys\Article\Type;
+use Biblys\Service\CurrentSite;
 use Biblys\Service\TemplateService;
+use Biblys\Test\ModelFactory;
 use PHPUnit\Framework\TestCase;
+use Propel\Runtime\Exception\PropelException;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+require_once __DIR__."/../../setUp.php";
 
 class InvitationControllerTest extends TestCase
 {
 
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws PropelException
+     * @throws LoaderError
+     */
     public function testNewAction()
     {
         // given
+        $publisher = ModelFactory::createPublisher();
+        ModelFactory::createArticle(publisher: $publisher, title: "Book");
+        $ebook = ModelFactory::createArticle(publisher: $publisher, title: "E-book", typeId: Type::EBOOK);
         $controller = new InvitationController();
+        $site = ModelFactory::createSite();
+        $currentSite = new CurrentSite($site);
+        $currentSite->setOption("publisher_filter", $publisher->getId());
         $templateService = $this->createMock(TemplateService::class);
         $templateService
             ->expects($this->once())
             ->method("render")
-            ->with("AppBundle:Invitation:new.html.twig")
+            ->with("AppBundle:Invitation:new.html.twig", [
+                "downloadableArticles" => [$ebook],
+            ])
             ->willReturn(new Response("Créer une invitation de téléchargement"));
 
         // when
-        $response = $controller->newAction($templateService);
+        $response = $controller->newAction($currentSite, $templateService);
 
         // then
         $this->assertEquals(200, $response->getStatusCode());
