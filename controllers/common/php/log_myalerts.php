d565d443b7184c96a30f9360223f49f03bf4c4a5
Clément Latzarus <c@iwzr.fr>
2024-05-06 15:55:30 +0200

    refactor(legacy): fix warnings and deprecations

--- controllers/common/php/log_myalerts.php
+++ controllers/common/php/log_myalerts.php
@@ -1,78 +1,77 @@
 <?php
 
-/** @noinspection HtmlUnknownTarget */
-/** @noinspection PhpUnhandledExceptionInspection */
-
-use Biblys\Legacy\LegacyCodeHelper;
-use Biblys\Service\Config;
 use Biblys\Service\CurrentSite;
 use Biblys\Service\CurrentUser;
+use Model\AlertQuery;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
-$config = Config::load();
-$currentSiteService = CurrentSite::buildFromConfig($config);
-$currentSite = $currentSiteService->getSite();
-if (!$currentSiteService->hasOptionEnabled("alerts")) {
-    throw new ResourceNotFoundException("Alerts are not enabled for this site");
-}
-/** @var Request $request */
-$currentUserService = CurrentUser::buildFromRequestAndConfig($request, $config);
-$currentUser = $currentUserService->getUser();
+/**
+ * @throws Exception
+ */
+return function (
+    CurrentSite $currentSiteService,
+    CurrentUser $currentUserService,
+    Request     $request,
+): Response|JsonResponse|RedirectResponse {
+    global $_SQL;
 
-$am = new AlertManager();
+    if (!$currentSiteService->hasOptionEnabled("alerts")) {
+        throw new ResourceNotFoundException("Alerts are not enabled for this site");
+    }
+    $currentSite = $currentSiteService->getSite();
+    $currentUser = $currentUserService->getUser();
 
-$result = array();
+    $am = new AlertManager();
 
-if ($request->getMethod() === "POST") {
-    $body = $request->getContent();
-    $params = json_decode($body, true);
-    
-    if (!LegacyCodeHelper::getGlobalVisitor()->hasAlert($params["article_id"])) {
-        /** @noinspection PhpUnhandledExceptionInspection */
-        $alert = $am->create();
+    $result = array();
 
-        $alert->set("user_id", $currentUser->getId());
-        $alert->set("site_id", $currentSite->getId());
-        $alert->set('article_id', $params["article_id"]);
+    if ($request->getMethod() === "POST") {
+        $body = $request->getContent();
+        $params = json_decode($body, true);
 
-        /** @noinspection PhpUnhandledExceptionInspection */
-        $am->update($alert);
-        $result['created'] = 1;
-    } else {
-        $alert = $am->get(array('user_id' => $currentUser->getId(), 'article_id' => $params["article_id"]));
+        $alert = AlertQuery::create()
+            ->filterBySite($currentSite)
+            ->filterByUser($currentUser)
+            ->filterByArticleId($params['article_id'])
+            ->findOne();
 
-        /** @noinspection PhpUnhandledExceptionInspection */
-        $am->delete($alert);
-        $result['deleted'] = 1;
+        if($alert) {
+            $alert->delete();
+            $result['deleted'] = 1;
+        } else {
+            $alert = new \Model\Alert();
+            $alert->setSite($currentSite);
+            $alert->setUser($currentUser);
+            $alert->setArticleId($params['article_id']);
+            $alert->save();
+
+            $result['created'] = 1;
+        }
+
+        return new JsonResponse($result);
     }
 
-    return new JsonResponse($result);
-}
+    $table = null;
 
-$table = null;
+    $request->attributes->set("page_title", "Mes alertes");
+    $content = null;
 
-\Biblys\Legacy\LegacyCodeHelper::setGlobalPageTitle("Mes alertes");
-
-$content = null;
-
-$alertToDeleteId = $request->query->get('del');
-if ($alertToDeleteId) {
-    $alert = $am->getById($alertToDeleteId);
-    if ($alert) {
-        /** @noinspection PhpUnhandledExceptionInspection */
-        $am->delete($alert);
+    $alertToDeleteId = $request->query->get('del');
+    if ($alertToDeleteId) {
+        $alert = $am->getById($alertToDeleteId);
+        if ($alert) {
+            $am->delete($alert);
+        }
+        return new RedirectResponse('/pages/log_myalerts?deleted=1');
     }
-    return new RedirectResponse('/pages/log_myalerts?deleted=1');
-}
 
-/** @var PDO $_SQL */
-$_SQL->query("SET SESSION sql_mode=''")->execute();
-/** @noinspection SqlCheckUsingColumns */
-$sql = $_SQL->prepare("
+    $_SQL->query("SET SESSION sql_mode=''")->execute();
+
+    $sql = $_SQL->prepare("
     SELECT `articles`.`article_id`, `article_title`, `article_url`, `article_authors`, `article_collection`, `article_number`, `article_publisher`,
     `alert_id`, `alert_pub_year`, `alert_max_price`, `alert_condition`,
     `stock_id`
@@ -88,48 +87,48 @@ $sql = $_SQL->prepare("
     GROUP BY `alert_id`
     ORDER BY `alert_id`, `stock_purchase_date`
 ");
-$sql->execute(['user_id' => $currentUser->getId(), 'site_id' => $currentSite->getId()]);
+    $sql->execute(['user_id' => $currentUser->getId(), 'site_id' => $currentSite->getId()]);
 
-while ($a = $sql->fetch(PDO::FETCH_ASSOC)) {
-    if ($a["alert_max_price"]) {
-        $a["price"] = price($a["alert_max_price"], 'EUR');
-    } else {
-        $a['price'] = null;
-    }
-    if ($a["stock_id"]) {
-        $a["led"] = '<img src="/common/img/square_green.png" alt="Disponible" title="Disponible !" />';
-    } else {
-        $a["led"] = '<img src="/common/img/square_red.png" alt="inisponible" />';
-    }
+    while ($a = $sql->fetch(PDO::FETCH_ASSOC)) {
+        if ($a["alert_max_price"]) {
+            $a["price"] = price($a["alert_max_price"], 'EUR');
+        } else {
+            $a['price'] = null;
+        }
+        if ($a["stock_id"]) {
+            $a["led"] = '<img src="/common/img/square_green.png" alt="Disponible" title="Disponible !" />';
+        } else {
+            $a["led"] = '<img src="/common/img/square_red.png" alt="inisponible" />';
+        }
 
-    $table .= '
+        $table .= '
         <tr>
-            <td class="center">'.$a["led"].'</td>
-            <td><a href="/'.$a["article_url"].'">'.$a["article_title"].'</a><br />'.authors($a["article_authors"]).'<br />'.$a["article_collection"].numero($a["article_number"]).' ('.$a["article_publisher"].')</td>
-            <td class="right">'.$a["alert_pub_year"].'</td>
-            <td class="right">'.$a["price"].'</td>
-            <td class="center">'.$a["alert_condition"].'</td>
-            <td class="center"><a href="?del='.$a["alert_id"].'"><img src="/common/icons/delete_16.png" alt="Supprimer" title="Supprimer cette alerte" data-confirm="Voulez-vous vraiment SUPPRIMER cette alerte ?" /></a></td>
+            <td class="center">' . $a["led"] . '</td>
+            <td><a href="/' . $a["article_url"] . '">' . $a["article_title"] . '</a><br />' . authors($a["article_authors"]) . '<br />' . $a["article_collection"] . numero($a["article_number"]) . ' (' . $a["article_publisher"] . ')</td>
+            <td class="right">' . $a["alert_pub_year"] . '</td>
+            <td class="right">' . $a["price"] . '</td>
+            <td class="center">' . $a["alert_condition"] . '</td>
+            <td class="center"><a href="?del=' . $a["alert_id"] . '"><img src="/common/icons/delete_16.png" alt="Supprimer" title="Supprimer cette alerte" data-confirm="Voulez-vous vraiment SUPPRIMER cette alerte ?" /></a></td>
         </tr>
     ';
-    $current_article = $a["article_id"];
-}
 
-$content .= '
+    }
+
+    $content .= '
     <h2>
         <span class="fa fa-bell-o"></span>
-        '.\Biblys\Legacy\LegacyCodeHelper::getGlobalPageTitle().'
+        Mes alertes
     </h2>
 
     <p><strong>Ci-dessous, la liste des alertes que vous avez créées.</strong></p>
     <p>Vous recevrez un courriel dès qu\'ils seront disponibles aux conditions que vous avez indiquées dans une des librairies Biblys.</p>
 ';
 
-if (isset($_GET["deleted"])) {
-    $content .= '<p class="success">L\'alerte pour ce livre a bien été supprimée !</p>';
-}
+    if (isset($_GET["deleted"])) {
+        $content .= '<p class="success">L\'alerte pour ce livre a bien été supprimée !</p>';
+    }
 
-$content .= '
+    $content .= '
     <table class="list">
         <thead>
             <tr>
@@ -142,9 +141,10 @@ $content .= '
             </tr>
         </thead>
         <tbody>
-            '.$table.'
+            ' . $table . '
         </tbody>
     </table>
 ';
 
-return new Response($content);
+    return new Response($content);
+};
