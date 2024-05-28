f451af39d180dccaf5f9b0f7a026b9dc680cbb20
Clément Latzarus <c@iwzr.fr>
2024-05-28 11:05:33 +0200

    feat(service): remove dead code

--- src/Biblys/Service/Log.php
+++ src/Biblys/Service/Log.php
@@ -23,33 +23,6 @@ class Log
     }
 
     /**
-     * @deprecated Legacy Log service is deprecated. Use LoggerService->log("mails", …) instead.
-     */
-    public static function mail(string $level, string $message, array $context = []): void
-    {
-        $logger = new LoggerService();
-        $logger->log('mails', $level, $message, $context);
-    }
-
-    /**
-     * @deprecated Legacy Log service is deprecated. Use LoggerService->log("security", …) instead.
-     */
-    public static function security(string $level, string $message, array $context = []): void
-    {
-        $logger = new LoggerService();
-        $logger->log('security', $level, $message, $context);
-    }
-
-    /**
-     * @deprecated Legacy Log service is deprecated. Use LoggerService->log("paypal", …) instead.
-     */
-    public static function paypal(string $level, string $message, array $context = []): void
-    {
-        $logger = new LoggerService();
-        $logger->log('paypal', $level, $message, $context);
-    }
-
-    /**
      * @deprecated Legacy Log service is deprecated. Use LoggerService->log("payplug", …) instead.
      */
     public static function payplug(string $level, string $message, array $context = []): void
