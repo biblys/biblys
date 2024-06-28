837c1d1f89bc0ae70ae732f25dfd09050b2d88d9
Clément Latzarus <c@ltzr.io>
2024-06-28 23:37:58 +0200

    refactor(entity): inject dependencies into method
    in Order->markAsPayed

--- src/Biblys/Test/ModelFactory.php
+++ src/Biblys/Test/ModelFactory.php
@@ -234,6 +234,7 @@ class ModelFactory
      */
     public static function createOrder(
         Site   $site = null,
+        User   $user = null,
         string $axysAccountId = null,
         string $slug = null,
     ): Order
