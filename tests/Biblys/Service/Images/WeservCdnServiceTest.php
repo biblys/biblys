a8a18ccbe4a641991d5fe1e7963da4f9cb82f89a
Clément Latzarus <c@ltzr.io>
2024-06-30 18:13:58 +0200

    feat(web): allow to resize images using CDN service

--- tests/Biblys/Service/Images/WeservCdnServiceTest.php
+++ tests/Biblys/Service/Images/WeservCdnServiceTest.php
@@ -6,7 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 class WeservCdnServiceTest extends TestCase
 {
-
     public function testBuildUrl()
     {
         // given
@@ -19,6 +18,40 @@ class WeservCdnServiceTest extends TestCase
         // then
         $this->assertEquals(
             "//images.weserv.nl/?url=https%3A%2F%2Fwww.biblys.fr%2Fimages%2F1955.jpg",
+            $url,
+            "builds url for Weserv CDN",
+        );
+    }
+
+    public function testBuildUrlWithWidth()
+    {
+        // given
+        $localUrl = "https://www.biblys.fr/images/1955.jpg";
+        $service = new WeservCdnService();
+
+        // when
+        $url = $service->buildUrl(url: $localUrl, width: 512);
+
+        // then
+        $this->assertEquals(
+            "//images.weserv.nl/?url=https%3A%2F%2Fwww.biblys.fr%2Fimages%2F1955.jpg&w=512",
+            $url,
+            "builds url for Weserv CDN",
+        );
+    }
+
+    public function testBuildUrlWithHeight()
+    {
+        // given
+        $localUrl = "https://www.biblys.fr/images/1955.jpg";
+        $service = new WeservCdnService();
+
+        // when
+        $url = $service->buildUrl(url: $localUrl, height: 768);
+
+        // then
+        $this->assertEquals(
+            "//images.weserv.nl/?url=https%3A%2F%2Fwww.biblys.fr%2Fimages%2F1955.jpg&h=768",
             $url,
             "builds url for Weserv CDN",
         );
