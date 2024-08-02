4922f9bf3058d653453b2c48f7b9391babb6fba8
Clément Latzarus <c@ltzr.io>
2024-08-02 18:20:09 +0200

    feat(cli): import images folder by folder

--- src/Command/ImportImagesCommand.php
+++ src/Command/ImportImagesCommand.php
@@ -16,11 +16,10 @@ use Symfony\Component\Finder\Finder;
 
 class ImportImagesCommand extends Command
 {
-    protected static $defaultName = 'images:import';
+    protected static $defaultName = "images:import";
 
     public function __construct(
         private readonly Config $config,
-        private readonly Finder $finder,
         private readonly Filesystem $filesystem,
         private readonly ImagesService $imagesService,
         string $name = null,
@@ -41,17 +40,32 @@ class ImportImagesCommand extends Command
     {
         $loggerService = new LoggerService();
 
-        $coversDirectory = $this->config->getImagesPath() . "/book";
+        $coversDirectory = $this->config->getImagesPath() . "book";
         $output->writeln(["Listing in files {$coversDirectory}…"]);
         $loggerService->log("images-import", "info", "Listing files {$coversDirectory}…");
 
-        $coversDirectoryFullPath = __DIR__ . "/../.." . $coversDirectory;
-        $coverFiles = $this->finder
-            ->in($coversDirectoryFullPath)
-            ->files()
-            ->depth(1);
+        $coverDirectories = [];
+        $coversDirectoryFullPath = __DIR__ . "/../../" . $coversDirectory;
+        $fileCount = 0;
+        for ($i = 0; $i < 100; $i++) {
+            $currentDirectoryName = str_pad($i, 2, "0", STR_PAD_LEFT);
+            $currentDirectory = $coversDirectoryFullPath . "/" . $currentDirectoryName;
+            $currentDirectoryPath = $coversDirectory . "/". $currentDirectoryName;
+            if (!$this->filesystem->exists($currentDirectory)) {
+                $output->writeln(["- Directory $currentDirectoryPath : —"]);
+                continue;
+            }
 
-        $fileCount = $coverFiles->count();
+            $finder = new Finder();
+            $coverFilesInCurrentDirectory = $finder
+                ->in($currentDirectory)
+                ->files();
+            $currentDirectoryFileCount = $coverFilesInCurrentDirectory->count();
+            $fileCount += $currentDirectoryFileCount;
+            $output->writeln(["- Directory $currentDirectoryPath : $currentDirectoryFileCount files"]);
+            $coverDirectories[] = $coverFilesInCurrentDirectory;
+        }
+
         $output->writeln(["$fileCount files to process"]);
         $loggerService->log("images-import", "info", "$fileCount files to process");
 
@@ -63,40 +77,42 @@ class ImportImagesCommand extends Command
         $deletedFilesCount = 0;
         $skippedFilesCount = 0;
         $loadedImagesCount = 0;
-        foreach ($coverFiles as $coverFile) {
-            $filePath = $coverFile->getRealPath();
-            preg_match_all('/book\/\d{2}\/(\d+)\.jpg/m', $filePath, $matches);
-            $articleId = $matches[1][0];
-            $article = ArticleQuery::create()->findPk($articleId);
+        foreach ($coverDirectories as $coverDirectoryFiles) {
+            foreach ($coverDirectoryFiles as $coverFile) {
+                $filePath = $coverFile->getRealPath();
+                preg_match_all('/book\/\d{2}\/(\d+)\.jpg/m', $filePath, $matches);
+                $articleId = $matches[1][0];
+                $article = ArticleQuery::create()->findPk($articleId);
 
-            if (!$article) {
-                $this->filesystem->remove($filePath);
-                $progress->setMessage("Deleted cover for inexistant article $articleId");
-                $loggerService->log("images-import", "info", "Deleted cover for inexistant article $articleId");
+                if (!$article) {
+                    $this->filesystem->remove($filePath);
+                    $progress->setMessage("Deleted cover for inexistant article $articleId");
+                    $loggerService->log("images-import", "info", "Deleted cover for inexistant article $articleId");
+                    $progress->advance();
+                    $deletedFilesCount++;
+                    continue;
+                }
+
+                if ($this->imagesService->imageExistsFor($article)) {
+                    $progress->setMessage("Skipped already imported cover for article $articleId ({$article->getTitle()})");
+                    $loggerService->log("images-import", "info", "Ignored already imported cover for article $articleId  ({$article->getTitle()})");
+                    $progress->advance();
+                    $skippedFilesCount++;
+                    continue;
+                }
+
+                $this->imagesService->addImageFor($article, $filePath);
+
+                $progress->setMessage("Imported cover for article $articleId ({$article->getTitle()})");
+                $loggerService->log("images-import", "info", "Imported cover for article $articleId ({$article->getTitle()})");
+                $loadedImagesCount++;
                 $progress->advance();
-                $deletedFilesCount++;
-                continue;
             }
-
-            if ($this->imagesService->imageExistsFor($article)) {
-                $progress->setMessage("Skipped already imported cover for article $articleId ({$article->getTitle()})");
-                $loggerService->log("images-import", "info", "Ignored already imported cover for article $articleId  ({$article->getTitle()})");
-                $progress->advance();
-                $skippedFilesCount++;
-                continue;
-            }
-
-            $this->imagesService->addImageFor($article, $filePath);
-
-            $progress->setMessage("Imported cover for article $articleId ({$article->getTitle()})");
-            $loggerService->log("images-import", "info", "Imported cover for article $articleId ({$article->getTitle()})");
-            $loadedImagesCount++;
-            $progress->advance();
         }
 
         $progress->finish();
 
-        $output->writeln(["Loaded $loadedImagesCount images, skipped $skippedFilesCount files and deleted $deletedFilesCount files."]);
+        $output->writeln(["", "Loaded $loadedImagesCount images, skipped $skippedFilesCount files and deleted $deletedFilesCount files."]);
         return 0;
     }
 }
