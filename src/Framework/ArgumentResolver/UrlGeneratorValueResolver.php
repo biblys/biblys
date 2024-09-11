54d79bf75e3a9dad6001d6b39b63ff837cb2577f
Clément Latzarus <c@ltzr.io>
2024-09-11 12:30:40 +0200

    refactor(framework): inject request context in url generator

--- src/Framework/ArgumentResolver/UrlGeneratorValueResolver.php
+++ src/Framework/ArgumentResolver/UrlGeneratorValueResolver.php
@@ -2,11 +2,13 @@
 
 namespace Framework\ArgumentResolver;
 
+use Framework\RouteLoader;
 use Generator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\RequestContext;
 
 class UrlGeneratorValueResolver implements ArgumentValueResolverInterface
 {
@@ -22,7 +24,8 @@ class UrlGeneratorValueResolver implements ArgumentValueResolverInterface
 
     public function resolve(Request $request, ArgumentMetadata $argument): Generator
     {
-        $container = include __DIR__."/../../container.php";
-        yield $container->get("url_generator");
+        $routes = RouteLoader::load();
+        $requestContext = RequestContext::fromUri($request->getUri());
+        yield new UrlGenerator($routes, $requestContext);
     }
 }
