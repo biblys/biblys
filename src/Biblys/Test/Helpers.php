7443f0ddb3cad5fb3b64794aea9249b2d7d10e67
Clément Latzarus <c@ltzr.io>
2024-10-20 16:28:45 +0200

    test: add template service test helper

--- src/Biblys/Test/Helpers.php
+++ src/Biblys/Test/Helpers.php
@@ -2,9 +2,16 @@
 
 namespace Biblys\Test;
 
+use Biblys\Service\Config;
+use Biblys\Service\CurrentSite;
+use Biblys\Service\CurrentUser;
+use Biblys\Service\MetaTagsService;
+use Biblys\Service\TemplateService;
 use Exception;
+use Mockery;
 use ReflectionClass;
 use ReflectionException;
+use Symfony\Component\HttpFoundation\Request;
 
 class Helpers
 {
@@ -35,5 +42,32 @@ class Helpers
         }
 
         throw new Exception("Excepted function to throw an exception, but none was.");
+    }
+
+    /**
+     * @return TemplateService
+     * @throws Exception
+     */
+    public static function getTemplateService(): TemplateService
+    {
+        $config = new Config(["environment" => "test"]);
+        $currentSite = Mockery::mock(CurrentSite::class);
+        $currentSite->expects("getTitle")->andReturn("Éditions Paronymie");
+        $currentSite->expects("getOption")->andReturn(null);
+        $currentUser = Mockery::mock(CurrentUser::class);
+        $currentUser->expects("isAuthentified")->andReturn(false);
+        $currentUser->expects("isAdmin")->andReturn(false);
+        $currentUser->expects("hasPublisherRight")->andReturn(false);
+        $metaTags = Mockery::mock(MetaTagsService::class);
+        $metaTags->expects("dump")->andReturn("");
+        $request = new Request();
+
+        return new TemplateService(
+            config: $config,
+            currentSiteService: $currentSite,
+            currentUserService: $currentUser,
+            metaTagsService: $metaTags,
+            request: $request
+        );
     }
 }
