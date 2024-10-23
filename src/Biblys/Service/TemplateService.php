ab989d048310c0e88d538b21a8f7df018f6ab07e
Clément Latzarus <c@ltzr.io>
2024-10-23 19:15:59 +0200

    feat(service): allow to use publisher in twig filters

--- src/Biblys/Service/TemplateService.php
+++ src/Biblys/Service/TemplateService.php
@@ -11,6 +11,7 @@ use Exception;
 use Framework\TemplateLoader;
 use Model\Article;
 use Model\Post;
+use Model\Publisher;
 use Model\Stock;
 use Propel\Runtime\Exception\PropelException;
 use Symfony\Bridge\Twig\Extension\FormExtension;
@@ -274,16 +275,17 @@ class TemplateService
 
         $imagesService = new ImagesService($config, $currentSite, new Filesystem());
 
-        $filters[] = new TwigFilter('hasImage', function (Article|Stock|Post $model) use($imagesService) {
-            return $imagesService->imageExistsFor($model);
-        });
+        $filters[] = new TwigFilter('hasImage',
+            fn (Article|Stock|Post|Publisher $model) => $imagesService->imageExistsFor($model)
+        );
 
         $filters[] = new TwigFilter('imageUrl',
-            function (Article|Stock|Post $model, array $options = []) use ($imagesService) {
-                $width = $options[0] ?? null;
-                $height = $options[1] ?? null;
-                return $imagesService->getImageUrlFor($model, width: $width, height: $height);
-            },
+            fn (Article|Stock|Post|Publisher $model, array $options = []) =>
+                $imagesService->getImageUrlFor(
+                    model: $model,
+                    width: $options[0] ?? null,
+                    height: $options[1] ?? null
+                ),
             ['is_variadic' => true]
         );
 
