a65344123c7522553a2be24eabac721b68cc5505
Clément Latzarus <c@ltzr.io>
2024-12-06 23:00:38 +0100

    refactor(service): use string service in slug service

--- src/Biblys/Service/Slug/SlugService.php
+++ src/Biblys/Service/Slug/SlugService.php
@@ -18,20 +18,17 @@
 
 namespace Biblys\Service\Slug;
 
+use Biblys\Service\StringService;
 use Symfony\Component\String\Slugger\AsciiSlugger;
-use Symfony\Component\String\UnicodeString;
 
 class SlugService
 {
     public function slugify(string $input): string
     {
-        $replacements = [
-            "&" => "et"
-        ];
+        $replacements = ["&" => "et"];
         $slugger = new AsciiSlugger("fr", ["fr" => $replacements]);
-        $string = new UnicodeString($input);
-        $lowercaseString = $string->lower();
-        return $slugger->slug($lowercaseString);
+
+        return $slugger->slug((new StringService($input))->lowercase()->get());
     }
 
     public function createForBookCollection($collectionName, $publisherName): string
