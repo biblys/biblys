3c304f40cdf71015165f8a8a160df7f3e469032b
Clément Latzarus <c@ltzr.io>
2025-01-02 18:24:55 +0100

    feat(service): add CacheService

---
+++ src/Framework/ArgumentResolver/CacheServiceValueResolver.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Copyright (C) 2024 Clément Latzarus
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+namespace Framework\ArgumentResolver;
+
+use Biblys\Service\CacheService;
+use Biblys\Service\Config;
+use Exception;
+use Generator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+class CacheServiceValueResolver implements ArgumentValueResolverInterface
+{
+
+    public function supports(Request $request, ArgumentMetadata $argument): bool
+    {
+        if ($argument->getType() !== CacheService::class) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function resolve(Request $request, ArgumentMetadata $argument): Generator
+    {
+        $config = Config::load();
+        yield new CacheService($config);
+    }
+}
