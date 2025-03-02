5a47fa22792d4fb8f34ca9bc8120847143d62564
Clément Latzarus <c@ltzr.io>
2025-03-02 10:30:05 +0100

    refactor(web): extract log and mail sending from usecase

--- src/Usecase/AddAdminUsecase.php
+++ src/Usecase/AddAdminUsecase.php
@@ -18,44 +18,25 @@
 
 namespace Usecase;
 
-use Biblys\Exception\InvalidEmailAddressException;
 use Biblys\Service\CurrentSite;
-use Biblys\Service\FlashMessagesService;
-use Biblys\Service\Mailer;
-use Biblys\Service\TemplateService;
 use Model\Right;
 use Model\RightQuery;
 use Model\User;
 use Model\UserQuery;
 use Propel\Runtime\Exception\PropelException;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
 
 class AddAdminUsecase
 {
-    public function __construct(
-        private readonly CurrentSite $currentSite,
-        private readonly FlashMessagesService $flashMessages,
-        private readonly UrlGeneratorInterface $urlGenerator,
-        private readonly TemplateService $templateService,
-        private readonly Mailer $mailer,
-    )
-    {}
+    public function __construct(private readonly CurrentSite $currentSite) {}
 
     /**
-     * @throws TransportExceptionInterface
-     * @throws RuntimeError
-     * @throws InvalidEmailAddressException
-     * @throws LoaderError
-     * @throws SyntaxError
      * @throws PropelException
      * @throws BusinessRuleException
      */
-    public function execute(string $userEmail): void
+    public function execute(string $userEmail): array
     {
+        $userWasCreated = false;
+
         $user = UserQuery::create()
             ->filterBySite($this->currentSite->getSite())
             ->findOneByEmail($userEmail);
@@ -63,7 +44,8 @@ class AddAdminUsecase
             $user = new User();
             $user->setEmail($userEmail);
             $user->setSite($this->currentSite->getSite());
-            $this->flashMessages->add("info", "Un compte utilisateur a été créé pour $userEmail.");
+            $user->save();
+            $userWasCreated = true;
         }
 
         $isUserAlreadyAdmin = RightQuery::create()->isUserAdmin($user);
@@ -77,35 +59,6 @@ class AddAdminUsecase
         $right->setIsAdmin(true);
         $right->save();
 
-        $adminUrl = $this->urlGenerator->generate("main_admin", [], UrlGeneratorInterface::ABSOLUTE_URL);
-        $adminEmailBody = $this->templateService->render("AppBundle:Admins:admin-welcome_email.html.twig", [
-            "email" => $userEmail,
-            "adminUrl" => $adminUrl,
-        ]);
-        $this->mailer->send(
-            to: $user->getEmail(),
-            subject: "Votre accès admin au site {$this->currentSite->getSite()->getTitle()}",
-            body: $adminEmailBody
-        );
-
-        $admins = RightQuery::create()
-            ->filterByIsAdmin(true)
-            ->joinWithUser()
-            ->find();
-        $adminsPageUrl = "https://{$this->currentSite->getSite()->getDomain()}/pages/adm_admins";
-        $alertEmailBody = $this->templateService->render("AppBundle:Admins:admin-added-alert_email.html.twig", [
-            "email" => $userEmail,
-            "adminsPageUrl" => $adminsPageUrl,
-        ]);
-        foreach ($admins as $admin) {
-            $adminEmail = $admin->getUser()->getEmail();
-            $this->mailer->send(
-                to: $adminEmail,
-                subject: "Alerte de sécurité : nouvel·le admin ajouté·e",
-                body: $alertEmailBody
-            );
-        }
-
-        $this->flashMessages->add("success", "Un accès administrateur a été ajouté pour le compte $userEmail.");
+        return [$userWasCreated, $user];
     }
 }
