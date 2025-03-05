9e37b906255827041aa9f4d2f788bc2b3f240fb7
Clément Latzarus <c@ltzr.io>
2025-03-05 08:05:29 +0100

    feat(web): add preview images to blog rss feed

--- tests/AppBundle/Controller/FeedControllerTest.php
+++ tests/AppBundle/Controller/FeedControllerTest.php
@@ -20,6 +20,7 @@ namespace AppBundle\Controller;
 
 use Biblys\Service\CurrentSite;
 use Biblys\Service\CurrentUrlService;
+use Biblys\Service\Images\ImagesService;
 use Biblys\Test\Helpers;
 use Biblys\Test\ModelFactory;
 use DateTime;
@@ -86,9 +87,10 @@ class FeedControllerTest extends TestCase
         $currentUrl->method("getAbsoluteUrl")->willReturn("https://example.com/feed");
         $urlGenerator = $this->createMock(UrlGenerator::class);
         $urlGenerator->method("generate")->willReturn("https://example.com/post/1");
+        $imagesService = $this->createMock(ImagesService::class);
 
         // when
-        $response = $controller->blogAction($currentSite, $currentUrl, $urlGenerator);
+        $response = $controller->blogAction($currentSite, $currentUrl, $urlGenerator, $imagesService);
 
         // then
         $this->assertEquals(200, $response->getStatusCode());
@@ -119,7 +121,12 @@ XML
         $controller = new FeedController();
         $site = ModelFactory::createSite();
 
-        ModelFactory::createPost(title: "Une actualité dans le flux", date: new DateTime("2019-04-28 02:42:00"));
+        $post = ModelFactory::createPost(
+            title: "Une actualité dans le flux",
+            date: new DateTime("2019-04-28 02:42:00"),
+            content: "<p>Un contenu d'actualité qui va vous étonner.</p>",
+        );
+        ModelFactory::createImage(post: $post, type: "illustration");
         ModelFactory::createPost(title: "Une actualité non publiée", status: 0);
         ModelFactory::createPost(title: "Une actualité du futur", date: new DateTime("+1 day"));
 
@@ -128,9 +135,12 @@ XML
         $currentUrl->method("getAbsoluteUrl")->willReturn("https://example.com/feed");
         $urlGenerator = $this->createMock(UrlGenerator::class);
         $urlGenerator->method("generate")->willReturn("https://example.com/post/1");
+        $imagesService = $this->createMock(ImagesService::class);
+        $imagesService->method("imageExistsFor")->willReturn(true);
+        $imagesService->method("getImageUrlFor")->willReturn("/images/post/1.jpg");
 
         // when
-        $response = $controller->blogAction($currentSite, $currentUrl, $urlGenerator);
+        $response = $controller->blogAction($currentSite, $currentUrl, $urlGenerator, $imagesService);
 
         // then
         $this->assertEquals(200, $response->getStatusCode());
@@ -150,9 +160,54 @@ XML
       <pubDate>Sun, 28 Apr 2019 02:42:00 +0000</pubDate>
       <link>https://example.com/post/1</link>
       <guid>https://example.com/post/1</guid>
-      <content:encoded><![CDATA[Un contenu d'actualité qui va vous étonner.]]></content:encoded>
+      <content:encoded><![CDATA[<img src="/images/post/1.jpg" alt="" role="presentation" /><p>Un contenu d'actualité qui va vous étonner.</p>]]></content:encoded>
       <slash:comments>0</slash:comments>
     </item>
+  </channel>
+</rss>
+
+XML
+            , $response->getContent());
+    }
+
+
+
+    /**
+     * @throws PropelException
+     * @throws Exception
+     */
+    public function testBlogActionWithEmptyPosts()
+    {
+        // given
+        $controller = new FeedController();
+        $site = ModelFactory::createSite();
+
+        ModelFactory::createPost(date: new DateTime("2019-04-28 02:42:00"), content: "");
+
+        $currentSite = new CurrentSite($site);
+        $currentUrl = $this->createMock(CurrentUrlService::class);
+        $currentUrl->method("getAbsoluteUrl")->willReturn("https://example.com/feed");
+        $urlGenerator = $this->createMock(UrlGenerator::class);
+        $urlGenerator->method("generate")->willReturn("https://example.com/post/1");
+        $imagesService = $this->createMock(ImagesService::class);
+        $imagesService->method("imageExistsFor")->willReturn(false);
+
+        // when
+        $response = $controller->blogAction($currentSite, $currentUrl, $urlGenerator, $imagesService);
+
+        // then
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("application/rss+xml", $response->headers->get("Content-Type"));
+        $this->assertEquals(<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Éditions Paronymie · Le blog</title>
+    <description>Les derniers billets du blog</description>
+    <pubDate>Sun, 28 Apr 2019 02:42:00 +0000</pubDate>
+    <generator>Biblys (https://biblys.org)</generator>
+    <link>https://paronymie.fr</link>
+    <atom:link rel="self" type="application/rss+xml" href="https://example.com/feed"/>
   </channel>
 </rss>
 
@@ -251,47 +306,6 @@ XML
       <content:encoded><![CDATA[<p>Ce livre paraît aujourd'hui.</p>]]></content:encoded>
       <slash:comments>0</slash:comments>
     </item>
-  </channel>
-</rss>
-
-XML
-            , $response->getContent());
-    }
-
-    /**
-     * @throws PropelException
-     * @throws Exception
-     */
-    public function testBlogActionWithEmptyPosts()
-    {
-        // given
-        $controller = new FeedController();
-        $site = ModelFactory::createSite();
-
-        ModelFactory::createPost(date: new DateTime("2019-04-28 02:42:00"), content: "");
-
-        $currentSite = new CurrentSite($site);
-        $currentUrl = $this->createMock(CurrentUrlService::class);
-        $currentUrl->method("getAbsoluteUrl")->willReturn("https://example.com/feed");
-        $urlGenerator = $this->createMock(UrlGenerator::class);
-        $urlGenerator->method("generate")->willReturn("https://example.com/post/1");
-
-        // when
-        $response = $controller->blogAction($currentSite, $currentUrl, $urlGenerator);
-
-        // then
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals("application/rss+xml", $response->headers->get("Content-Type"));
-        $this->assertEquals(<<<XML
-<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-  <channel>
-    <title>Éditions Paronymie · Le blog</title>
-    <description>Les derniers billets du blog</description>
-    <pubDate>Sun, 28 Apr 2019 02:42:00 +0000</pubDate>
-    <generator>Biblys (https://biblys.org)</generator>
-    <link>https://paronymie.fr</link>
-    <atom:link rel="self" type="application/rss+xml" href="https://example.com/feed"/>
   </channel>
 </rss>
 
