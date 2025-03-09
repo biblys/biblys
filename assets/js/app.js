35bcca5692dccd1af72b2f4f031c60a5f65feaf3
Clément Latzarus <c@ltzr.io>
2025-03-09 17:47:02 +0100

    feat(assets): remove swiper library

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,6 @@ import '../css/app.css';
 import '../css/overall-menu.css';
 
 document.addEventListener('DOMContentLoaded', function() {
-  loadModuleForElements('carousel', '.biblys-carousel');
   loadModuleForElements('lightbox', '[rel=lightbox]');
   loadModuleForElements('readmore', '[data-readmore]');
 });
