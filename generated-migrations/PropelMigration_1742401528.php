39e5cbe48696c0282b9eb4f0d699c50eb0981e01
Clément Latzarus <c@ltzr.io>
2025-03-19 17:24:42 +0100

    feat(model): add ShippingZone model

---
+++ generated-migrations/PropelMigration_1742401528.php
@@ -0,0 +1,152 @@
+<?php
+/*
+ * Copyright (C) 2025 Clément Latzarus
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use Propel\Generator\Manager\MigrationManager;
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1742401528.
+ * Generated on 2025-03-19 16:25:28 by clement */
+class PropelMigration_1742401528{
+    /**
+     * @var string
+     */
+    public $comment = '';
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function preUp(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function postUp(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function preDown(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function postDown(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL(): array
+    {
+        $connection_default = <<< 'EOT'
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+ALTER TABLE `shipping`
+
+  ADD `shipping_zone_id` int unsigned AFTER `shipping_zone`;
+
+CREATE INDEX `shipping_fi_ce7733` ON `shipping` (`shipping_zone_id`);
+
+CREATE TABLE `shipping_zones`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(128) NOT NULL,
+    `description` VARCHAR(512),
+    `created_at` TIMESTAMP NULL,
+    `updated_at` TIMESTAMP NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `shipping_zones_countries`
+(
+    `shipping_zone_id` INTEGER NOT NULL,
+    `country_id` INTEGER NOT NULL,
+    PRIMARY KEY (`shipping_zone_id`,`country_id`),
+    INDEX `shipping_zones_countries_fi_6005d8` (`country_id`)
+) ENGINE=InnoDB;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+EOT;
+
+        return [
+            'default' => $connection_default,
+        ];
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL(): array
+    {
+        $connection_default = <<< 'EOT'
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `shipping_zones`;
+
+DROP TABLE IF EXISTS `shipping_zones_countries`;
+
+
+DROP INDEX `shipping_fi_ce7733` ON `shipping`;
+
+ALTER TABLE `shipping`
+
+  DROP `shipping_zone_id`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+EOT;
+
+        return [
+            'default' => $connection_default,
+        ];
+    }
+
+}
