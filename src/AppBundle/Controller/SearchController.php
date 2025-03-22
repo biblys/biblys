e6e5a31cc071587693be29c50204e3976d67d703
Clément Latzarus <c@ltzr.io>
2025-03-22 15:49:54 +0100

    feat(web): add search controller

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -4,8 +4,10 @@ namespace AppBundle\Controller;
 
 use Biblys\Service\CurrentSite;
 use Biblys\Service\CurrentUser;
+use Biblys\Service\QueryParamsService;
 use Framework\Controller;
 use Loupe\Loupe\Loupe;
+use Loupe\Loupe\SearchParameters;
 use Model\Article;
 use Model\ArticleQuery;
 use Propel\Runtime\Exception\PropelException;
@@ -55,6 +57,32 @@ class SearchController extends Controller
         }
 
         return new Response($articles->count()." articles indexed");
+    }
+
+    public function searchAction(QueryParamsService $queryParams): Response
+    {
+        $queryParams->parse(["q" => ["type" => "string"]]);
+        $q = $queryParams->get("q");
+
+        $searchParameters = SearchParameters::create()
+            ->withQuery($q)
+            ->withAttributesToRetrieve(["title", "contributors"])
+            ->withSort(["title:asc"])
+            ->withHitsPerPage(100)
+            ->withPage(1)
+        ;
+
+        $loupe = $this->_getLoupeClient();
+        $results = $loupe->search($searchParameters);
+
+        $content = "";
+
+        foreach ($results->getHits() as $hit) {
+            $contributors = !empty($hit["contributors"]) ? join(", ", $hit["contributors"]) : "";
+            $content .= $hit["title"] . " - " . $contributors . "<br>";
+        }
+
+        return new Response($content);
     }
 
     /**
