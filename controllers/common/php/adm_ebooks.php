13a882e3fe3eb312c0678dbd57d4b0aadc45172a
Clément Latzarus <c@ltzr.io>
2025-03-28 18:12:11 +0100

    refactor(legacy): fix warnings and deprecations

--- controllers/common/php/adm_ebooks.php
+++ controllers/common/php/adm_ebooks.php
@@ -16,69 +16,81 @@
  */
 
 
-use Biblys\Legacy\LegacyCodeHelper;
 use Biblys\Service\CurrentSite;
+use Propel\Runtime\Exception\PropelException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-/** @var PDO $_SQL */
-/** @var Request $request */
-/** @var CurrentSite $currentSite */
+/**
+ * @throws InvalidDateFormatException
+ * @throws PropelException
+ */
+return function(Request $request, CurrentSite $currentSite): Response
+{
+    $request->attributes->set("page_title", "Ventes numériques");
 
-$request->attributes->set("page_title", "Ventes numériques");
+    if (!$currentSite->getOption('downloadable_publishers')) {
+        throw new Exception("L'option de site `downloadable_publishers` doit être définie.");
+    }
 
-if (!$currentSite->getOption('downloadable_publishers')) {
-    throw new Exception("L'option de site `downloadable_publishers` doit être définie.");
-}
+    // Article select
+    $ao = array();
+    $articles = EntityManager::prepareAndExecute("SELECT `article_id`, `article_title`, `article_collection` FROM `articles`
+        WHERE
+            `publisher_id` IN (" . $currentSite->getOption('downloadable_publishers') . ")
+            AND (`type_id` = 2 OR `type_id` = 11) ORDER BY `article_collection`, `article_title_alphabetic`",
+        []
+    );
+    while ($a = $articles->fetch(PDO::FETCH_ASSOC)) {
+        $ao[$a["article_collection"]][] = '<option value="' . $a["article_id"] . '"' . (isset($_GET["article_id"]) && $_GET["article_id"] == $a["article_id"] ? ' selected' : null) . '>' . $a["article_title"] . '</option>';
+    }
 
-// Article select
-$ao = array();
-$articles = $_SQL->query("SELECT `article_id`, `article_title`, `article_collection` FROM `articles`
-    WHERE
-        `publisher_id` IN (" . $currentSite->getOption('downloadable_publishers') . ")
-        AND (`type_id` = 2 OR `type_id` = 11) ORDER BY `article_collection`, `article_title_alphabetic`");
-while ($a = $articles->fetch(PDO::FETCH_ASSOC)) {
-    $ao[$a["article_collection"]][] = '<option value="' . $a["article_id"] . '"' . (isset($_GET["article_id"]) && $_GET["article_id"] == $a["article_id"] ? ' selected' : null) . '>' . $a["article_title"] . '</option>';
-}
+    $article_options = NULL;
+    foreach ($ao as $c => $a) {
+        $article_options .= '<optgroup label="' . $c . '">' . implode($a) . '</optgroup>';
+    }
 
-$article_options = NULL;
-foreach ($ao as $c => $a) {
-    $article_options .= '<optgroup label="' . $c . '">' . implode($a) . '</optgroup>';
-}
+    // People select
+    $people_options = NULL;
+    $people = EntityManager::prepareAndExecute("
+        SELECT `people_id`, `people_name` 
+        FROM `articles` 
+        JOIN `roles` USING(`article_id`) 
+        JOIN `people` USING(`people_id`) 
+        JOIN `collections` USING(`collection_id`) 
+        WHERE `collections`.`site_id` = :site_id AND `type_id` = 2 AND `job_id` = 1 
+        GROUP BY `people_id` 
+        ORDER BY MAX(`people_last_name`)
+    ", ["site_id" => $currentSite->getSite()->getId()]);
+    while ($p = $people->fetch(PDO::FETCH_ASSOC)) {
+        $people_options .= '<option value="' . $p["people_id"] . '">' . $p["people_name"] . '</option>';
+    }
 
-// People select
-$people_options = NULL;
-$people = $_SQL->prepare("SELECT `people_id`, `people_name` FROM `articles` JOIN `roles` USING(`article_id`) JOIN `people` USING(`people_id`) JOIN `collections` USING(`collection_id`) WHERE `collections`.`site_id` = :site_id AND `type_id` = 2 AND `job_id` = 1 GROUP BY `people_id` ORDER BY `people_last_name`");
-$people->execute(['site_id' => $currentSite->getSite()->getId()]);
-while ($p = $people->fetch(PDO::FETCH_ASSOC)) {
-    $people_options .= '<option value="' . $p["people_id"] . '">' . $p["people_name"] . '</option>';
-}
+    $req = NULL;
+    $reqPeople = null;
+    $reqParams = ['site_id' => $currentSite->getSite()->getId()];
+    $reqPeopleParams = [];
 
-$req = NULL;
-$reqPeople = null;
-$reqParams = ['site_id' => $currentSite->getSite()->getId()];
-$reqPeopleParams = [];
+    if (!empty($_GET["date1"])) {
+        $req .= " AND `stock_selling_date` >= :date1 AND `stock_selling_date` <= :date2";
+        $reqParams['date1'] = $request->query->get('date1') . ' 00:00:00';
+        $reqParams['date2'] = $request->query->get('date2') . ' 23:59:59';
+    }
 
-if (!empty($_GET["date1"])) {
-    $req .= " AND `stock_selling_date` >= :date1 AND `stock_selling_date` <= :date2";
-    $reqParams['date1'] = $request->query->get('date1') . ' 00:00:00';
-    $reqParams['date2'] = $request->query->get('date2') . ' 23:59:59';
-}
+    $articleId = $request->query->get('article_id');
+    if ($articleId) {
+        $req .= " AND `stock`.`article_id` = :article_id";
+        $reqParams['article_id'] = $articleId;
+    }
 
-$articleId = $request->query->get('article_id');
-if ($articleId) {
-    $req .= " AND `stock`.`article_id` = :article_id";
-    $reqParams['article_id'] = $articleId;
-}
+    $peopleId = $request->query->get('people_id');
+    if ($peopleId) {
+        $reqPeople = " AND `article_links` LIKE :people_id";
+        $reqPeopleParams['people_id'] = '%[people:' . $peopleId . ']%';
+    }
 
-$peopleId = $request->query->get('people_id');
-if ($peopleId) {
-    $reqPeople = " AND `article_links` LIKE :people_id";
-    $reqPeopleParams['people_id'] = '%[people:' . $peopleId . ']%';
-}
-
-// Ventes numériques
-$ventes = EntityManager::prepareAndExecute("
+    // Ventes numériques
+    $ventes = EntityManager::prepareAndExecute("
     SELECT
         `article_id`, `article_title`, `article_url`, `article_authors`, `article_ean`,
         `article_publisher`, `article_authors`, `article_price`
@@ -92,35 +104,40 @@ $ventes = EntityManager::prepareAndExecute("
     GROUP BY `article_id`
     ORDER BY `article_authors_alphabetic` ", array_merge($reqParams, $reqPeopleParams));
 
-$tbody = NULL;
-$Ventes = null;
-$Total = null;
-$Gratuits = null;
-$subReqParams = $reqParams;
-while ($l = $ventes->fetch(PDO::FETCH_ASSOC)) {
+    $tbody = NULL;
+    $Ventes = null;
+    $Total = null;
+    $Gratuits = null;
+    $subReqParams = $reqParams;
+    while ($l = $ventes->fetch(PDO::FETCH_ASSOC)) {
 
-    if (!empty($_GET["people_id"])) LegacyCodeHelper::setGlobalPageTitle("Ventes numériques : " . authors($l["article_authors"]));
-    if (!empty($_GET["article_id"])) LegacyCodeHelper::setGlobalPageTitle("Ventes numériques : " . $l["article_title"]);
+        if (!empty($_GET["people_id"])) {
+            $request->attributes->set("page_title", "Ventes numériques : " . authors($l["article_authors"]));
+        }
 
-    $subReqParams['article_id'] = $l['article_id'];
+        if (!empty($_GET["article_id"])) {
+            $request->attributes->set("page_title", "Ventes numériques : {$l["article_title"]}");
+        }
 
-    $numVentes = $_SQL->prepare("
-        SELECT COUNT(`stock_id`) AS `ventes`, SUM(`stock_selling_price`) AS `ca`, SUM(`stock_selling_price_ht`) AS `ca_ht` 
-        FROM `stock` 
-        WHERE `article_id` = :article_id 
-            AND `stock_selling_price` != '0' 
-            AND `stock_selling_date` 
-            AND `site_id` = :site_id " . $req . " 
-            AND (`stock`.`axys_account_id` IS NOT NULL OR `stock`.`user_id` IS NOT NULL)
-        ");
-    $numVentes->execute($subReqParams);
+        $subReqParams['article_id'] = $l['article_id'];
+
+        $numVentes = EntityManager::prepareAndExecute("
+            SELECT COUNT(`stock_id`) AS `ventes`, SUM(`stock_selling_price`) AS `ca`, SUM(`stock_selling_price_ht`) AS `ca_ht` 
+            FROM `stock` 
+            WHERE `article_id` = :article_id 
+                AND `stock_selling_price` != '0' 
+                AND `stock_selling_date` 
+                AND `site_id` = :site_id " . $req . " 
+                AND (`stock`.`axys_account_id` IS NOT NULL OR `stock`.`user_id` IS NOT NULL)
+            ", $subReqParams);
     $v = $numVentes->fetch(PDO::FETCH_ASSOC);
 
-    $numGratuits = $_SQL->prepare("SELECT COUNT(`stock_id`) AS `gratuits` FROM `stock` WHERE `article_id` = :article_id AND `stock_selling_price` IS NULL AND `site_id` = :site_id " . $req);
-    $numGratuits->execute($subReqParams);
-    $g = $numGratuits->fetch(PDO::FETCH_ASSOC);
+        $numGratuits = EntityManager::prepareAndExecute(
+            "SELECT COUNT(`stock_id`) AS `gratuits` FROM `stock` WHERE `article_id` = :article_id AND `stock_selling_price` IS NULL AND `site_id` = :site_id " . $req,
+            $subReqParams);
+        $g = $numGratuits->fetch(PDO::FETCH_ASSOC);
 
-    $tbody .= '
+        $tbody .= '
         <tr>
             <td>' . authors($l["article_authors"]) . '</td>
             <td><a href="/a/' . $l["article_url"] . '">' . $l["article_title"] . '</a></td>
@@ -129,14 +146,14 @@ while ($l = $ventes->fetch(PDO::FETCH_ASSOC)) {
             <td class="right">' . price($v["ca"], 'EUR') . '</td>
         </tr>
     ';
-    $Total += $v["ca"];
-    $Ventes += $v["ventes"];
-    $Gratuits += $g["gratuits"];
-}
+        $Total += $v["ca"];
+        $Ventes += $v["ventes"];
+        $Gratuits += $g["gratuits"];
+    }
 
-$content = '<h1><span class="fa fa-book"></span> Ventes numériques</h1>';
+    $content = '<h1><span class="fa fa-book"></span> Ventes numériques</h1>';
 
-$content .= '
+    $content .= '
         <form class="fieldset" role="form">
             <fieldset>
                 <legend>Filter les ventes</legend>
@@ -182,98 +199,100 @@ $content .= '
         </form>
         <br />
 
-    <h3>Par titre</h3>
-    <br />
-
-    <table class="admin-table sortable">
-        <thead>
-            <tr class="cliquable">
-                <th>Auteur</th>
-                <th>Titre</th>
-                <th class="right">Ventes</th>
-                <th class="right">Gratuits</th>
-                <th class="right">C.A.&nbsp;total</th>
-            </tr>
-        </thead>
-        <tbody>
-            ' . $tbody . '
-        </tbody>
-        <tfoot>
-            <tr>
-                <td></td>
-                <td class="right">Total :</td>
-                <td class="right">' . $Ventes . '</td>
-                <td class="right">' . $Gratuits . '</td>
-                <td class="right">' . price($Total, 'EUR') . '</td>
-            </tr>
-        </tfoot>
-    </table>
-';
-
-$articleId = $request->query->get('article_id');
-if ($articleId) {
-    $content .= '<h3>Statistiques de téléchargements</h3>';
-
-    $downloads = $_SQL->prepare("SELECT `download_filetype`, `download_version` FROM `downloads` WHERE `article_id` = :article_id");
-    $downloads->execute(['article_id' => $articleId]);
-    $total = $downloads->rowCount();
-
-    $content .= '
+        <h3>Par titre</h3>
+        <br />
+    
         <table class="admin-table sortable">
-            <tr>
-                <td class="right">Total :</td>
-                <td class="right">' . $total . '</td>
-                <td class="right">100 %</td>
-            </tr>
+            <thead>
+                <tr class="cliquable">
+                    <th>Auteur</th>
+                    <th>Titre</th>
+                    <th class="right">Ventes</th>
+                    <th class="right">Gratuits</th>
+                    <th class="right">C.A.&nbsp;total</th>
+                </tr>
+            </thead>
+            <tbody>
+                ' . $tbody . '
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td></td>
+                    <td class="right">Total :</td>
+                    <td class="right">' . $Ventes . '</td>
+                    <td class="right">' . $Gratuits . '</td>
+                    <td class="right">' . price($Total, 'EUR') . '</td>
+                </tr>
+            </tfoot>
         </table>
     ';
-}
 
-$content .= '<h3>Toutes les ventes</h3>';
+    $articleId = $request->query->get('article_id');
+    if ($articleId) {
+        $content .= '<h3>Statistiques de téléchargements</h3>';
 
-$achats = $_SQL->prepare("SELECT 
-    `article_title`, 
-    `users`.`email`,
-    `stock`.`axys_account_id`,
-    `stock_selling_price`,`stock_selling_date`, `stock_id`
-    FROM `articles`
-    JOIN `stock` USING(`article_id`)
-    LEFT JOIN `users` ON `users`.`id` = `stock`.`user_id`
-    WHERE `stock`.`site_id` = :site_id AND (`type_id` = '2' OR `type_id` = 11) " .$req.$reqPeople."
-    GROUP BY `stock_id`
-ORDER BY `stock_selling_date` DESC");
-$achats->execute(array_merge($reqParams, $reqPeopleParams));
+        $downloads = EntityManager::prepareAndExecute(
+            "SELECT `download_filetype`, `download_version` FROM `downloads` WHERE `article_id` = :article_id",
+            ['article_id' => $articleId]
+        );
+        $total = $downloads->rowCount();
 
-$content .= '<br />
-    <table class="admin-table sortable">
-        <thead>
-            <tr class="cliquable">
-                <th>Ref.</th>
-                <th>Quand&nbsp;?</th>
-                <th>Quoi&nbsp;?</th>
-                <th>Qui&nbsp;?</th>
-                <th>Combien&nbsp;?</th>
-            </tr>
-        </thead>
-        <tbody>
-';
+        $content .= '
+            <table class="admin-table sortable">
+                <tr>
+                    <td class="right">Total :</td>
+                    <td class="right">' . $total . '</td>
+                    <td class="right">100 %</td>
+                </tr>
+            </table>
+        ';
+    }
 
-$customers = [];
-while ($a = $achats->fetch(PDO::FETCH_ASSOC)) {
-    $userIdentity = $a["email"] ?: "Utilisateur Axys n°" . $a["axys_account_id"];
+    $content .= '<h3>Toutes les ventes</h3>';
 
-    $content .= '
-        <tr>
-            <td>' . $a["stock_id"] . '</td>
-            <td>' . _date($a['stock_selling_date'], "j/m/Y") . '</td>
-            <td>' . $a['article_title'] . '</td>
-            <td>' . $userIdentity . '</td>
-            <td class="right">' . price($a['stock_selling_price'], 'EUR') . '</td>
-        </tr>
+    $achats = EntityManager::prepareAndExecute("SELECT 
+        `article_title`, 
+        `users`.`email`,
+        `stock`.`axys_account_id`,
+        `stock_selling_price`,`stock_selling_date`, `stock_id`
+        FROM `articles`
+        JOIN `stock` USING(`article_id`)
+        LEFT JOIN `users` ON `users`.`id` = `stock`.`user_id`
+        WHERE `stock`.`site_id` = :site_id AND (`type_id` = '2' OR `type_id` = 11) " . $req . $reqPeople . "
+        GROUP BY `stock_id`
+        ORDER BY `stock_selling_date` DESC",
+        array_merge($reqParams, $reqPeopleParams)
+    );
+
+    $content .= '<br />
+        <table class="admin-table sortable">
+            <thead>
+                <tr class="cliquable">
+                    <th>Ref.</th>
+                    <th>Quand&nbsp;?</th>
+                    <th>Quoi&nbsp;?</th>
+                    <th>Qui&nbsp;?</th>
+                    <th>Combien&nbsp;?</th>
+                </tr>
+            </thead>
+            <tbody>
     ';
-    $customers[] = $userIdentity . ', ';
-}
 
-$content .= '</tbody></table>';
+    while ($a = $achats->fetch(PDO::FETCH_ASSOC)) {
+        $userIdentity = $a["email"] ?: "Utilisateur Axys n°" . $a["axys_account_id"];
 
-return new Response($content);
+        $content .= "
+            <tr>
+                <td>" . $a["stock_id"] . "</td>
+                <td>" . _date($a['stock_selling_date'], "j/m/Y") . "</td>
+                <td>" . $a['article_title'] . "</td>
+                <td>" . $userIdentity . "</td>
+                <td class=\"right\">" . price($a['stock_selling_price'], 'EUR') . "</td>
+            </tr>
+        ";
+    }
+
+    $content .= '</tbody></table>';
+
+    return new Response($content);
+};
