a125a5be33fd0a89bc1bc50314e4fa294a23f30b
Clément Latzarus <c@ltzr.io>
2025-04-09 15:47:16 +0200

    feat(legacy): remove site filtering in post category

--- inc/Category.class.php
+++ inc/Category.class.php
@@ -16,27 +16,15 @@
  */
 
 
-    class Category extends Entity
-    {
-        protected $prefix = 'category';
-    }
+class Category extends Entity
+{
+    protected $prefix = 'category';
+}
 
-    class CategoryManager extends EntityManager
-    {
-        protected $prefix = 'category',
-                  $table = 'categories',
-                  $object = 'Category';
+class CategoryManager extends EntityManager
+{
+    protected $prefix = 'category',
+              $table = 'categories',
+              $object = 'Category';
 
-
-        public function create(array $defaults = array())
-        {
-            if (!isset($defaults['site_id'])) $defaults['site_id'] = $this->site['site_id'];
-            return parent::create($defaults);
-        }
-
-        public function getAll(array $where = array(), array $options = array(), $withJoins = true)
-        {
-            $where['categories`.`site_id'] = $this->site['site_id'];
-            return parent::getAll($where, $options);
-        }
-    }
+}
