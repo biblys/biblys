6a0a0daa8f1cf05f5412314dc8f70345132a57b0
Clément Latzarus <c@ltzr.io>
2025-05-21 08:51:39 +0200

    feat(db): migrate "suivi" to "colissimo"

---
+++ generated-migrations/PropelMigration_1747809646.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * Copyright (C) 2025 Clément Latzarus
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use Propel\Generator\Manager\MigrationManager;
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1747809646.
+ * Generated on 2025-05-21 06:40:46 by clement */
+class PropelMigration_1747809646{
+    /**
+     * @var string
+     */
+    public $comment = '';
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function preUp(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function postUp(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function preDown(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function postDown(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL(): array
+    {
+        $connection_default = <<< 'EOT'
+UPDATE `orders` SET `order_shipping_mode` = 'colissimo' WHERE `order_shipping_mode` = 'suivi';
+UPDATE `shipping` SET `shipping_type` = 'colissimo' WHERE `shipping_type` = 'suivi';
+EOT;
+
+        return [
+            'default' => $connection_default,
+        ];
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL(): array
+    {
+        $connection_default = <<< 'EOT'
+UPDATE `orders` SET `order_shipping_mode` = 'suivi' WHERE `order_shipping_mode` = 'colissimo';
+UPDATE `shipping` SET `shipping_type` = 'suivi' WHERE `shipping_type` = 'colissimo';
+EOT;
+
+        return [
+            'default' => $connection_default,
+        ];
+    }
+
+}
