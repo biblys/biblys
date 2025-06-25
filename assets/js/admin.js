d03a75d67f630eb9df0ca4f0a7ab77179a587d06
Clément Latzarus <c@ltzr.io>
2025-06-25 08:20:13 +0200

    feat(web): remove legacy entity search

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -21,7 +21,6 @@ import '../css/admin.css';
 document.addEventListener('DOMContentLoaded', function() {
   loadModuleForElements('admin-shortcuts/admin-shortcuts-manager', '#shortcuts-form');
   loadModuleForElements('cron-task', '.cron-execute-task');
-  loadModuleForElements('entity-search', '[data-entity_search]');
   loadModuleForElements('orders/orders-manager', '#orders');
   loadModuleForElements('search-terms-manager', '#searchable-articles-table');
   loadModuleForElements('shipping', '#shipping-table');
