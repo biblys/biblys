946f625aca0c5babbef1a05b7b9fb44ca000eeef
Clément Latzarus <c@ltzr.io>
2025-08-14 20:20:58 +0200

    feat(db): add migration to recreate relations between article and tags

---
+++ generated-migrations/PropelMigration_1755195185.php
@@ -0,0 +1,94 @@
+<?php
+use Propel\Generator\Manager\MigrationManager;
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1755195185.
+ * Generated on 2025-08-14 18:13:05 by clement */
+class PropelMigration_1755195185{
+    /**
+     * @var string
+     */
+    public $comment = '';
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function preUp(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function postUp(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function preDown(MigrationManager $manager)
+    {
+        // add the pre-migration code here
+    }
+
+    /**
+     * @param \Propel\Generator\Manager\MigrationManager $manager
+     *
+     * @return null|false|void
+     */
+    public function postDown(MigrationManager $manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL(): array
+    {
+        $connection_default = <<< 'EOT'
+
+INSERT INTO tags_articles (article_id, tag_id)
+SELECT article_id, tag_id
+FROM links
+WHERE article_id IS NOT NULL AND tag_id IS NOT NULL;
+
+DELETE FROM links WHERE article_id IS NOT NULL AND tag_id IS NOT NULL;
+
+EOT;
+
+        return [
+            'default' => $connection_default,
+        ];
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL(): array
+    {
+        $connection_default = <<< 'EOT'
+EOT;
+
+        return [
+            'default' => $connection_default,
+        ];
+    }
+
+}
