fc98bee9437597acabb2d32939c2e02b83644b7c
Clément Latzarus <c@ltzr.io>
2025-09-03 11:50:07 +0200

    feat(web): allow to filter sales by year

--- tests/AppBundle/Controller/StatsControllerTest.php
+++ tests/AppBundle/Controller/StatsControllerTest.php
@@ -20,11 +20,13 @@ namespace AppBundle\Controller;
 
 use Biblys\Service\Config;
 use Biblys\Service\CurrentUser;
+use Biblys\Service\QueryParamsService;
 use Biblys\Test\Helpers;
 use Biblys\Test\ModelFactory;
 use DateTime;
 use Exception;
 use Mockery;
+use Model\StockQuery;
 use PHPUnit\Framework\TestCase;
 use Propel\Runtime\Exception\PropelException;
 use Twig\Error\LoaderError;
@@ -33,6 +35,14 @@ use Twig\Error\SyntaxError;
 
 class StatsControllerTest extends TestCase
 {
+    /**
+     * @throws PropelException
+     */
+    public function setUp(): void
+    {
+        StockQuery::create()->deleteAll();
+    }
+
     /**
      * @throws SyntaxError
      * @throws RuntimeError
@@ -50,17 +60,59 @@ class StatsControllerTest extends TestCase
 
         $currentUser = Mockery::mock(CurrentUser::class);
         $currentUser->shouldReceive("authAdmin")->andReturn();
+        $queryParams = Mockery::mock(QueryParamsService::class);
+        $queryParams->shouldReceive("parse")->with([
+            "year" => [
+                "type" => "numeric",
+                "default" => date("Y"),
+                "mb_min_length" => 4,
+                "mb_max_length" => 4,
+            ]
+        ])->andReturn();
         $templateService = Helpers::getTemplateService();
+        $queryParams->shouldReceive("getInteger")->with("year")->andReturn(date("Y"));
 
         // when
-        $response = $controller->salesByArticleAction($currentUser, $templateService);
+        $response = $controller->salesByArticleAction($currentUser, $queryParams ,$templateService);
 
         // then
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertStringContainsString("Sold article", $response->getContent());
         $this->assertStringContainsString("24,68", $response->getContent());
     }
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws PropelException
+     * @throws LoaderError
+     * @throws Exception
+     */
+    public function testSalesByArticleActionWithYear(): void
+    {
+        // given
+        $article = ModelFactory::createArticle(title: "Article sold in 2022");
+        ModelFactory::createStockItem(article: $article, sellingPrice: 1234, sellingDate: new DateTime("2022-05-01"));
+        $article = ModelFactory::createArticle(title: "Article sold in 2023");
+        ModelFactory::createStockItem(article: $article, sellingPrice: 1234, sellingDate: new DateTime("2023-05-01"));
+        ModelFactory::createStockItem(article: $article, sellingPrice: 1234, sellingDate: new DateTime("2023-06-01"));
+        $controller = new StatsController();
 
+        $currentUser = Mockery::mock(CurrentUser::class);
+        $currentUser->shouldReceive("authAdmin")->andReturn();
+        $queryParams = Mockery::mock(QueryParamsService::class);
+        $queryParams->shouldReceive("parse")->andReturn();
+        $queryParams->shouldReceive("getInteger")->with("year")->andReturn(2023);
+        $templateService = Helpers::getTemplateService();
+
+        // when
+        $response = $controller->salesByArticleAction($currentUser, $queryParams, $templateService);
+
+        // then
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString("Article sold in 2023", $response->getContent());
+        $this->assertStringContainsString("24,68", $response->getContent());
+        $this->assertStringNotContainsString("Article sold in 2022", $response->getContent());
+    }
 
     /**
      * @throws Exception
