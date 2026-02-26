bda8caf6b696bfe3e8f9b39f2a05989153437682
Clément Latzarus <c@ltzr.net>
2026-02-26 10:41:29 +0100

    refactor(legacy): improve variable names

--- controllers/common/php/adm_noosfere_import.php
+++ controllers/common/php/adm_noosfere_import.php
@@ -28,77 +28,56 @@ return function(): JsonResponse
 {
     $slugService = new SlugService();
 
-    $x = [];
-    if (!empty($_GET["noosfere_id"])) { // Importation de fiche noosfere
-        $noosfere = new Noosfere();
-        $xml = $noosfere->search($_GET["noosfere_id"], 'noosfere_id');
-        $n = Noosfere::buildArticlesFromXml($xml);
-        $n = $n[0];
-        if (!empty($n["article_ean"])) { // Infos complémentaires d'Amazon si EAN
-            if (!empty($a)) {
-                foreach ($a as $key => $val) { // Champ à récupérer d'Amazon uniquement si vide chez noosfere
-                    if (empty($n[$key])) {
-                        $n[$key] = $val;
-                    }
-                }
-            }
-            // Champ à récupérer en priorité chez Amazon
-            if (isset($a["article_pubdate"])) {
-                $n["article_pubdate"] = $a["article_pubdate"];
-            }
-            if (isset($a["article_cover_import"])) {
-                $n["article_cover_import"] = $a["article_cover_import"];
-            }
-            if (isset($a["article_price"])) {
-                $n["article_price"] = $a["article_price"];
-            }
-        }
-        $x = $n;
-    }
+    $noosfereId = $_GET["noosfere_id"];
 
-    $publisher = Noosfere::getOrCreatePublisher($x["noosfere_IdEditeur"], $x["article_publisher"]);
-    $x["publisher_id"] = $publisher->getId();
+    $noosfere = new Noosfere();
+    $xml = $noosfere->search($noosfereId, 'noosfere_id');
+    $importedArticles = Noosfere::buildArticlesFromXml($xml);
+    $articleToCreate = $importedArticles[0];
+
+    $publisher = Noosfere::getOrCreatePublisher($articleToCreate["noosfere_IdEditeur"], $articleToCreate["article_publisher"]);
+    $articleToCreate["publisher_id"] = $publisher->getId();
 
     $collection = Noosfere::getOrCreateCollection(
-        $x["noosfere_IdCollection"],
-        $x["article_collection"],
-        PublisherQuery::create()->findPk($x["publisher_id"]),
+        $articleToCreate["noosfere_IdCollection"],
+        $articleToCreate["article_collection"],
+        PublisherQuery::create()->findPk($articleToCreate["publisher_id"]),
     );
-    $x["collection_id"] = $collection->getId();
-    $x["article_collection"] = $collection->getName();
-    $x["article_publisher"] = $collection->getPublisherName();
-    $x["pricegrid_id"] = $collection->getPricegridId();
+    $articleToCreate["collection_id"] = $collection->getId();
+    $articleToCreate["article_collection"] = $collection->getName();
+    $articleToCreate["article_publisher"] = $collection->getPublisherName();
+    $articleToCreate["pricegrid_id"] = $collection->getPricegridId();
 
     // Reconnaissance de cycle
-    if (!empty($x["article_cycle"])) {
+    if (!empty($articleToCreate["article_cycle"])) {
         $cym = new CycleManager();
-        $cycle = $cym->get(array('cycle_noosfere_id' => $x["noosfere_IdSerie"]));
+        $cycle = $cym->get(array('cycle_noosfere_id' => $articleToCreate["noosfere_IdSerie"]));
         if (!$cycle) {
-            $cycle = $cym->get(array('cycle_name' => $x["article_cycle"]));
+            $cycle = $cym->get(array('cycle_name' => $articleToCreate["article_cycle"]));
             if (!$cycle) {
-                $x["cycle_url"] = $slugService->slugify($x["article_cycle"]);
+                $articleToCreate["cycle_url"] = $slugService->slugify($articleToCreate["article_cycle"]);
                 $cycle = $cym->create(array(
-                    'cycle_name' => $x['article_cycle'],
-                    'cycle_url' => $slugService->slugify($x["article_cycle"]),
-                    'cycle_noosfere_id' => $x["noosfere_IdSerie"]
+                    'cycle_name' => $articleToCreate['article_cycle'],
+                    'cycle_url' => $slugService->slugify($articleToCreate["article_cycle"]),
+                    'cycle_noosfere_id' => $articleToCreate["noosfere_IdSerie"]
                 ));
             }
         }
 
-        $x['article_cycle'] = $cycle->get('name');
-        $x['cycle_id'] = $cycle->get('id');
+        $articleToCreate['article_cycle'] = $cycle->get('name');
+        $articleToCreate['cycle_id'] = $cycle->get('id');
     }
 
     // Reconnaissance des contributeurs
-    unset($x["article_authors"]);
+    unset($articleToCreate["article_authors"]);
     $authorsCount = 0;
-    if (!empty($x["article_people"])) {
-        $x["article_people"] = array_unique($x["article_people"], SORT_REGULAR);
-        foreach ($x["article_people"] as $k => $c) {
+    if (!empty($articleToCreate["article_people"])) {
+        $articleToCreate["article_people"] = array_unique($articleToCreate["article_people"], SORT_REGULAR);
+        foreach ($articleToCreate["article_people"] as $key => $c) {
 
             if (!empty($c["people_role"])) {
                 $job = Noosfere::getJobFromNoosfereName($c["people_role"]);
-                $x["article_people"][$k]["job_id"] = $job->getId();
+                $articleToCreate["article_people"][$key]["job_id"] = $job->getId();
                 if ($job->getId() === 1) {
                     $authorsCount++;
                 }
@@ -114,15 +93,15 @@ return function(): JsonResponse
                 noosfereContributorLastName: $c["people_last_name"],
             );
 
-            $x["article_people"][$k]["people_id"] = $contributor->getId();
-            $x["article_people"][$k]["people_name"] = $contributor->getFullName();
+            $articleToCreate["article_people"][$key]["people_id"] = $contributor->getId();
+            $articleToCreate["article_people"][$key]["people_name"] = $contributor->getFullName();
 
             // S'il manque des infos, on n'ajoute pas le contributeur au livre
-            if (empty($x["article_people"][$k]["people_id"])
-                || empty($x["article_people"][$k]["people_name"])
-                || empty($x["article_people"][$k]["job_id"])
+            if (empty($articleToCreate["article_people"][$key]["people_id"])
+                || empty($articleToCreate["article_people"][$key]["people_name"])
+                || empty($articleToCreate["article_people"][$key]["job_id"])
             ) {
-                unset($x["article_people"][$k]);
+                unset($articleToCreate["article_people"][$key]);
             }
         }
     }
@@ -132,7 +111,7 @@ return function(): JsonResponse
     $anonymousAuthorId = $currentSite->getOption("anonymous_author_id");
     if ($authorsCount === 0 && $anonymousAuthorId) {
         $anonymousAuthor = PeopleQuery::create()->findPk($anonymousAuthorId);
-        $x["article_people"][] = [
+        $articleToCreate["article_people"][] = [
             "people_id" => $anonymousAuthor->getId(),
             "people_first_name" => $anonymousAuthor->getFirstName(),
             "people_last_name" => $anonymousAuthor->getLastName(),
@@ -144,21 +123,21 @@ return function(): JsonResponse
     }
 
     // Reconnaissance des catégories
-    if (isset($x["pricegrid_id"]) && isset($x["article_price"])) {
+    if (isset($articleToCreate["pricegrid_id"]) && isset($articleToCreate["article_price"])) {
         /** @noinspection SqlResolve */
         $prices = EntityManager::prepareAndExecute(
             "SELECT `price_cat` FROM `prices`
             WHERE `price_amount` = :price_amount AND
                 `pricegrid_id` = :pricegrid_id LIMIT 1",
             [
-                'price_amount' => $x['article_price'],
-                'pricegrid_id' => $x['pricegrid_id']
+                'price_amount' => $articleToCreate['article_price'],
+                'pricegrid_id' => $articleToCreate['pricegrid_id']
             ]
         );
         if ($p = $prices->fetch(PDO::FETCH_ASSOC)) {
-            $x["article_category"] = $p["price_cat"];
+            $articleToCreate["article_category"] = $p["price_cat"];
         }
     }
 
-    return new JsonResponse($x);
+    return new JsonResponse($articleToCreate);
 };
