56b30eb4cbbcf5a12154a79439b7bd5a5ae37edd
Clément Latzarus <c@ltzr.net>
2026-03-29 15:12:26 +0200

    refactor: rename command to "site:configure"

--- tests/Usecase/ConfigureSiteUsecaseTest.php
+++ tests/Usecase/ConfigureSiteUsecaseTest.php
@@ -2,22 +2,14 @@
 
 namespace Usecase;
 
-use Biblys\Test\Helpers;
 use Biblys\Test\ModelFactory;
 use Exception;
 use Model\SiteQuery;
 use PHPUnit\Framework\TestCase;
 use Propel\Runtime\Exception\PropelException;
 
-class CreateSiteUsecaseTest extends TestCase
+class ConfigureSiteUsecaseTest extends TestCase
 {
-    /**
-     * @throws PropelException
-     */
-    protected function setUp(): void
-    {
-        SiteQuery::create()->deleteAll();
-    }
 
     /**
      * @throws PropelException
@@ -27,7 +19,7 @@ class CreateSiteUsecaseTest extends TestCase
     {
         // given
         ModelFactory::createSite(title: "Existing site", domain: "https://existing-site.com");
-        $usecase = new CreateSiteUsecase();
+        $usecase = new ConfigureSiteUsecase();
 
         // when
         $exception = Helpers::runAndCatchException(fn() =>
@@ -46,7 +38,7 @@ class CreateSiteUsecaseTest extends TestCase
     public function testUsecaseSucceeds()
     {
         // given
-        $usecase = new CreateSiteUsecase();
+        $usecase = new ConfigureSiteUsecase();
 
         // when
         $createdSite = $usecase->execute(siteName: "New site", baseUrl: "https://new-site.com", contactEmail: "contact@new-site.com");
