fe1e72778a97860a50519fa07b10982f8608cdd2
Clément Latzarus <c@ltzr.net>
2026-06-22 07:08:30 +0200

    refactor: replace WishManager and WishlistManager with Propel models and remove legacy classes

--- controllers/common/php/log_wishlist.php
+++ controllers/common/php/log_wishlist.php
@@ -18,6 +18,7 @@
 
 use Biblys\Legacy\LegacyCodeHelper;
 use Biblys\Service\CurrentUser;
+use Model\WishlistQuery;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -26,37 +27,35 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 /** @var CurrentUser $currentUser */
 /** @var Session $session */
 
-$wm = new WishlistManager();
-
-$wishlist = $wm->getById($request->query->get('id'));
+$wishlist = WishlistQuery::create()->findPk($request->query->get('id'));
 if (!$wishlist) {
     throw new NotFoundHttpException("Liste d'envie #{$request->query->get('id')} inconnue.");
 }
 
-if ($wishlist->get('user_id') !== $currentUser->getUser()->getId()) {
+if ($wishlist->getUserId() !== $currentUser->getUser()->getId()) {
     throw new AccessDeniedHttpException("Vous n'avez pas le droit de modifier cette liste d'envies.");
 }
 
 if ($request->getMethod() == "POST") {
 
-    $wishlist->set('wishlist_name', $request->request->get('wishlist_name'));
-    $wm->update($wishlist);
+    $wishlist->setName($request->request->get('wishlist_name'));
+    $wishlist->save();
 
-    $session->getFlashbag()->add('success', "Votre liste d'envies &laquo;&nbsp;" . $wishlist->get('name') . "&nbsp;&raquo; a bien été modifiée !");
+    $session->getFlashbag()->add('success', "Votre liste d'envies &laquo;&nbsp;" . $wishlist->getName() . "&nbsp;&raquo; a bien été modifiée !");
     redirect('/pages/log_mywishes');
 
 }
 
-LegacyCodeHelper::setGlobalPageTitle("Modifier &laquo;&nbsp;" . $wishlist->get('name') . "&nbsp;&raquo;");
+LegacyCodeHelper::setGlobalPageTitle("Modifier &laquo;&nbsp;" . $wishlist->getName() . "&nbsp;&raquo;");
 
 $_ECHO .= '
         <h2>' . LegacyCodeHelper::getGlobalPageTitle() . '</h2>
-    
+
         <form class="fieldset" method="post">
             <fieldset>
                 <div class="form-group">
                     <label for="wishlist_name" class="after">Nom de la liste :</label>
-                    <input type="text" class="form-control" value="' . $wishlist->get('name') . '" name="wishlist_name" id="wishlist_name">
+                    <input type="text" class="form-control" value="' . $wishlist->getName() . '" name="wishlist_name" id="wishlist_name">
                 </div>
                 <div class="text-center">
                     <button type="submit" class="btn btn-primary">Enregistrer</button>
