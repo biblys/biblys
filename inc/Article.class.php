84027660d68014fda3c3b5ecee37dcc5dca3b0f3
Clément Latzarus <c@ltzr.net>
2026-09-02 14:29:56 +0200

    fix: include type_id and article_price in available-stock search fields
    _buildSearchQueryForAvailableStock restricted selected columns, omitting
    type_id and article_price. Article.model.price then resolved to null,
    which loosely equals 0 in PHP, routing the cart button template into the
    free-download branch where article.type.isDownloadable crashed since
    type_id was never loaded.

--- inc/Article.class.php
+++ inc/Article.class.php
@@ -1343,7 +1343,7 @@ class ArticleManager extends EntityManager
         $searchCriteria = implode(" AND ", $queryWithParams["query"]);
         $stockCriteria = " AND `stock_selling_date` IS NULL AND `stock_return_date` IS NULL AND `stock_lost_date` IS NULL";
 
-        $options["fields"] = "`articles`.`article_id`, `article_url`, `article_title`, `article_authors`, `publisher_id`, `collection_id`, `cycle_id`, `article_tome`";
+        $options["fields"] = "`articles`.`article_id`, `article_url`, `article_title`, `article_authors`, `publisher_id`, `collection_id`, `cycle_id`, `article_tome`, `type_id`, `article_price`";
         $options["join"] = [["table" => "stock", "key" => "article_id"]];
         $options["group-by"] = "article_id";
 
